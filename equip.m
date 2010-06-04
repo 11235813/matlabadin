b136e6b93fc7cc9d1a0760df4fa678db885267f5
tlitp0 <tlitp0@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-06-04 21:55:37 +0000

    nitpicks

--- equip.m
+++ equip.m
@@ -9,9 +9,9 @@ function [item] = equip(iref)
 %equip() first checks for the type of input argument.  For a numeric input,
 %it loads idb(iref) and returns the structure (the stats) as item.  For a
 %text input, it searches through idb until it finds the item number with
-%a name string that matches iref, and then returns the stats.  Note that
-%this means it is (MUCH)^N (where N is large) faster to reference an item
-%by inum than by its name.
+%a name string that matches iref, and then returns the stats.
+%IMPORTANT : Note that it is much faster to reference an item
+%by inum (item_id, as parsed by armory) than by its name. /IMPORTANT
 
 global idb
 
@@ -26,7 +26,7 @@ if ischar(iref)
     item=idb(gi);
    else
        %Oops, we typed 'Last Laff'
-       error(['equip failed to find item ' iref ' in item database'])
+       error(['equip_call failed to find item ' iref ' in gear database'])
    end
    
 %numeric input (iref=40402)   
