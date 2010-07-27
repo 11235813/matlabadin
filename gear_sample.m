1d7a4075a9843682ce3c2e977ca284cecf05ffa5
theckhd <theckhd@gmail.com>
2010-07-27 18:58:43 +0000

    More updates: gear_sample.m - added a default set of mostly ICC 277 gear and enchants gear_db.m - added all the items used in gear_sample.m sample_calculation.m - added a "clear" to the first line stats_recalc.m - changed 'SP' to 'sp' to keep with convention

--- gear_sample.m
+++ gear_sample.m
@@ -2,73 +2,73 @@
 
 %%Items (1-17)
 %head
-egs(1)=equip(2);
+egs(1)=equip(51266);  %Sanctified Lightsworn Faceguard
 %neck
-egs(2)=equip(2);
+egs(2)=equip(50682);
 %shoulders
-egs(3)=equip(3);
+egs(3)=equip(51269);
 %back
-egs(4)=equip(4);
+egs(4)=equip(50466);
 %chest
-egs(5)=equip(5);
+egs(5)=equip(51265);
 %wrists
-egs(6)=equip(3);
+egs(6)=equip(51901);
 %hands
-egs(7)=equip(2);
+egs(7)=equip(51267);
 %waist
-egs(8)=equip(4);
+egs(8)=equip(50991);
 %legs
-egs(9)=equip(2);
+egs(9)=equip(51268);
 %feet
-egs(10)=equip(4);
+egs(10)=equip(50625);
 %finger0
-egs(11)=equip(5);
+egs(11)=equip(50622);
 %finger1
-egs(12)=equip(3);
+egs(12)=equip(50404);
 %trinket0
-egs(13)=equip(2);
+egs(13)=equip(50356);
 %trinket1
-egs(14)=equip(2);
+egs(14)=equip(54571);
 %main hand
-egs(15)=equip(40345);
+egs(15)=equip(50708);
 %off hand
-egs(16)=equip(16);
+egs(16)=equip(50729);
 %ranged
-egs(17)=equip(4);
+egs(17)=equip(50461);
 
 
 %%Enchants (21-37)
 %head
-egs(21)=equip(2);
+egs(21)=equip(44150);
 %neck
-egs(22)=equip(2);
+% egs(22)=equip(2);
 %shoulders
-egs(23)=equip(3);
+egs(23)=equip(44136);
 %back
-egs(24)=equip(4);
+egs(24)=equip(39001);
 %chest
-egs(25)=equip(5);
+egs(25)=equip(39005);
 %wrists
-egs(26)=equip(3);
+egs(26)=equip(44947);
 %hands
-egs(27)=equip(2);
+egs(27)=equip(38376);
 %waist
-egs(28)=equip(4);
+% egs(28)=equip(4);
 %legs
-egs(29)=equip(2);
+egs(29)=equip(38373);
 %feet
-egs(30)=equip(4);
+egs(30)=equip(39006);
 %finger0
-egs(31)=equip(5);
+egs(31)=equip(50);
 %finger1
-egs(32)=equip(3);
+egs(32)=equip(50);
 %trinket0
-egs(33)=equip(2);
+% egs(33)=equip(2);
 %trinket1
-egs(34)=equip(2);
+% egs(34)=equip(2);
 %main hand
-egs(35)=equip(3);
+egs(35)=equip(59619);
 %off hand
-egs(36)=equip(16);
+egs(36)=equip(38945);
 %ranged
-egs(37)=equip(4);
+% egs(37)=equip(4);
