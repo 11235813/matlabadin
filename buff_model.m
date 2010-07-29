f22565048253d998c7a6fa7e16d07987cd6d895d
tlitp0 <tlitp0@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-07-29 10:10:35 +0000

    - changed pc_model to a function (consistent with npc) - nitpicks in npc_model - first iteration of buff_model - adjusted stats_recalc, as to comply with the new versions

---
+++ buff_model.m
@@ -0,0 +1,34 @@
+%% Buffs/Debuffs
+%TODO insert comment later on
+
+%% Buffs
+buff.BoK=1;               %5% stats, resistance : Blessing of Kings / Mark of the Wild
+buff.SoE=1;               %agility&strength : Strength of Earth / Horn of Winter / Battle Shout
+buff.PWF=1;               %stamina : Power word Fortitude / Commanding Shout
+buff.ArcInt=1;            %mana : Arcane Intellect / Fel Intelligence
+buff.UnRage=1;            %multiplicative AP : Unleashed Rage / Blessing of Might / Trueshot Aura / Abomination's Might
+buff.FMT=1;               %lesser multiplicative SP : Flametongue Totem
+buff.ToW=1;               %greater multiplicative SP : Totem of Wrath / Demonic Pact
+buff.HePr=1;              %global hit : Heroic Presence (Draenei) /TODO check it up later
+buff.SwRet=1;             %global damage dealt : Swift Retribution / Ferocious Inspiration / Arcane Empowerment
+buff.LotP=1;              %global crit : Leader of the Pack / Rampage / Moonkin Aura / Elemental Oath / Honor Among Thieves
+buff.WF=1;                %physical haste : Windfury Totem / Improved Icy Talons / Swift Retribution
+buff.WoA=1;               %spell haste : Wrath of Air Totem / Improved Moonkin Form
+buff.BL=0;                %active haste : Bloodlust / Heroism / Time Warp
+buff.Devo=1;              %armor : Devotion Aura / Stoneskin Totem
+buff.Thorns=1;            %Thorns
+buff.RA=1;                %Retribution Aura
+buff.AW=0;                %Avenging Wrath (disabled by default)
+buff.JotW=1;              %replenishment : Judgements of the Wise / Hunting Party / Improved Soul Leech
+buff.DivPlea=1;           %Divine Plea
+buff.Focus=0;             %Focus Magic (disabled by default)
+
+%% Debuffs
+buff.SavCom=1;            %physical damage taken : Savage Combat / Blood Frenzy / Brittle Bones
+buff.Hemo=1;              %bleed damage taken : Mangle / Trauma / Hemorrhage
+buff.CoE=1;               %spell damage taken : Ebon Plaguebringer / Earth and Moon / Curse of the Elements / Master Poisoner 
+buff.ISB=1;               %spell crit : Improved Scorch / Improved Shadowbolt
+buff.Sund=1;              %multiplicative armor : Sunder Armor / Expose Armor / Faerie Fire / Scorpid Venom
+
+%% Player debuffs
+Chill_up=0; %PLACEHOLDER
--- player_model.m
+++ player_model.m
@@ -1,3 +1,4 @@
+function [base] = player_model(varargin)
 %% PLAYER_MODEL
 %This file contains all of the base player stats and formulas.  I.e.
 %anything that comes before talents and gear.
@@ -9,8 +10,42 @@
 %variables is to obfuscate things we don't frequently vary or care about.
 %Stuff we do care about (like the final stats) will probably still get
 %their own variables so that it's easy to do matrix operations on them.
+
+%player_model is now a function, having race as input argument :
+%race=1/2/3/4, corresponding to human/dwarf/draenai/blood elf (respectively)
+%If race is not specified, player_model will default to human.
+
 %% global variables
 global base
+
+%% global variables
+global base
+
+%% input handling
+for i = 1 : 2 : length(varargin)
+
+    name = varargin{i};
+    value = varargin{i+1};
+    switch name
+        case 'race'
+            race=value;
+    end
+end
+
+%% default (human)
+if exist('race')==0 race=1; end;
+
+%%racial base stats (lvl 80)
+%the format is      STR-STA-AGI-INT-SPI
+player_base_stats=[ 151 143  90  98 108;
+                    153 146  86  97 104;
+                    152 142  87  99 107;
+                    148 141  92 102 104];
+base.str=player_base_stats(race,1);
+base.sta=player_base_stats(race,2);
+base.agi=player_base_stats(race,3);
+base.int=player_base_stats(race,4);    
+base.spi=player_base_stats(race,5);
 
 base.level=80;
 base.wskill=5.*base.level; %inherent
@@ -26,20 +61,4 @@ base.phcrit=3.27;          %class and level dependent
 base.health=6934;          %class and level dependent
 base.mana=4394;            %class and level dependent
 base.exp=0;                %overridden in stats_recalc based on weapon type
-
-%% race master switch
-base.race=1; %human by default (1-human, 2-dwarf, 3-draenai, 4-blood elf)
-
-%%base stats block (for lvl 80 paladins, duh)
-%the format is      STR-STA-AGI-INT-SPI
-player_base_stats=[ 151 143  90  98 108;
-                    153 146  86  97 104;
-                    152 142  87  99 107;
-                    148 141  92 102 104];
-
-base.str=player_base_stats(base.race,1);
-base.sta=player_base_stats(base.race,2);
-base.agi=player_base_stats(base.race,3);
-base.int=player_base_stats(base.race,4);    
-base.spi=player_base_stats(base.race,5);
-
+end
