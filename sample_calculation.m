35e92748286e2733cca4935b8609ed18c8d9c009
theckhd <theckhd@gmail.com>
2010-07-29 13:16:32 +0000

    Moved a few redundant things from stats_recalc.m to npc_model.m Made race a field of the returned structure from player_model.m Uncommented arpen conversion in stat_conversions.m (needed in stats_recalc.m) Bugfixes in stats_recalc.m, mostly correcting variable names so that sample_calculation.m properly executes Added buff_model call to the sample_calculation.m file and implemented the new player_model function call

--- sample_calculation.m
+++ sample_calculation.m
@@ -2,8 +2,8 @@ clear;
 %load gear database
 gear_db;
 
-%invoke player model
-player_model
+%invoke player model.  1=human, 2=dwarf, 3=draenei, 4=belf
+base=player_model('race',1);
 
 %invoke npc model
 npc=npc_model('lvl',83);
@@ -16,4 +16,9 @@ gear_sample;
 
 %calculate relevant stats
 gear_stats
+
+%activate buffs
+buff_model
+
+%calculate final stats
 stats_recalc
