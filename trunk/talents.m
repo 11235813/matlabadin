e2e6c4cf40e4fecd2f2e142032e49efda4a4fef9
tlitp0 <tlitp0@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-08-06 20:29:40 +0000

    - updated to b12694 - minor fixes

--- trunk/talents.m
+++ trunk/talents.m
@@ -45,11 +45,12 @@ talent.HammeroftheRighteous=1;
 talent.WrathoftheLightbringer=2;
 
 talent.Reckoning=2;
+talent.HolyShield=1;
 talent.GrandCrusader=2;
 talent.DivineGuardian=1;
 
 talent.Vindication=2;
-talent.HolyShield=1;
+talent.ShieldoftheRighteous=1;
 talent.GuardedbytheLight=0;
 
 talent.ShieldoftheTemplar=3;
@@ -62,8 +63,8 @@ prot=zeros(7,4);
 prot(1,1)=talent.ProtectoroftheInnocent;prot(1,2)=talent.SealsofthePure;prot(1,3)=talent.ImprovedHammerofJustice;
 prot(2,1)=talent.JudgementsoftheJust;prot(2,2)=talent.Toughness;prot(2,3)=talent.GuardiansFavor;
 prot(3,1)=talent.HallowedGround;prot(3,2)=talent.Sanctuary;prot(3,3)=talent.HammeroftheRighteous;prot(3,4)=talent.WrathoftheLightbringer;
-prot(4,1)=talent.Reckoning;prot(4,2)=talent.GrandCrusader;prot(4,3)=talent.DivineGuardian;
-prot(5,1)=talent.Vindication;prot(5,2)=talent.HolyShield;prot(5,3)=talent.GuardedbytheLight;
+prot(4,1)=talent.Reckoning;prot(4,2)=talent.HolyShield;prot(4,3)=talent.GrandCrusader;prot(4,4)=talent.DivineGuardian;
+prot(5,1)=talent.Vindication;prot(5,2)=talent.ShieldoftheRighteous;prot(5,3)=talent.GuardedbytheLight;
 prot(6,1)=talent.ShieldoftheTemplar;prot(6,2)=talent.SacredDuty;
 prot(7,1)=talent.ArdentDefender;
 protpoints=sum(sum(prot));
