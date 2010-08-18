1e5063339eb94cc2cbf84a1032d97392e5b3f069
theckhd@gmail.com <theckhd@gmail.com@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-08-18 14:15:57 +0000

    Wrote a simple rotation drawing module

---
+++ trunk/rotation_drawer.m
@@ -0,0 +1,74 @@
+function [ output_args ] = rotation_drawer( rs , figno)
+%ROTATION_DRAWER Creates a graphical representation of a cast sequence
+%   The input to this function is a structure "rs" with the following
+%   fields:
+%rs.seq     is an integer array representing the sequence of ability usage.
+%           example: rs.seq=[1 0 2 3 1 2 4 1 2 3 1 2 3].  0 indicates an
+%           empty GCD
+%rs.names   is a cell array of strings representing the names of the
+%           abilities in the sequence.  ex: {'CS','Jud','Exo',...}
+%rs.cds     is an integer array containing the cooldowns of the abilities.
+%           ex: rs.cds=[4.5 8 15 15]
+%rs.order   is an optional integer array representing the display order of
+%           the abilities - the earlier abilities are drawn first.  ex:
+%           rs.order=[2 1 3 4 5] would reverse the draw order of the first
+%           two abilities
+%
+%An optional second input "figno" can be added to specify the figure number
+%of the output.
+
+
+
+if isfield(rs,'order')==0 || max(rs.order)~=max(rs.seq)
+    error('Order not defined or too short, defaulting to text order')
+    rs.order=[1:max(rs.seq)]
+end
+
+if length(rs.cds)<max(rs.seq)
+    error('One or more cooldowns unspecified, defaulting to 0')
+    rs.cds(length(rs.cds)+1:max(rs.seq))=0;
+end
+
+time=1.5.*[0:length(rs.seq)-1];
+rs.labels=rs.names(rs.order);
+rs.labels={'Empty' rs.labels{:}};
+rs.labels=fliplr(rs.labels);
+if exist('figno')
+    figure(figno)
+else
+    figure
+end
+% plot(time,-rs.seq,'s', ...
+%     'MarkerSize',40, ...
+%     'MarkerFaceColor',[1 0 0],...
+%     'MarkerEdgeColor','k',...
+%     'LineWidth',2)
+plot(time,zeros(size(rs.seq)),'k-')
+ylim([-max(rs.seq) 1])
+set(gca,'XTick',0:1.5:max(time), ...
+        'XGrid','on', ...
+        'YTick',[-max(rs.seq):1:0]+0.5, ...
+        'YTickLabels',rs.labels);
+
+for m=1:length(rs.seq)
+    %create cooldown rectangle
+    if rs.seq(m)>0 && rs.cds(rs.seq(m))>0
+    rectangle('Position',[time(m) -rs.order(rs.seq(m)) rs.cds(rs.seq(m)) 0.6],...
+              'LineWidth',1,...
+              'FaceColor',[0 1 0])
+    end
+    %create ability use rectangle for non-empties
+    if rs.seq(m)>0
+    rectangle('Position',[time(m) -rs.order(rs.seq(m)) 1.5 0.8], ...
+              'LineWidth',2, ...
+              'FaceColor',[1 0 0])
+    %now for empties
+    else
+    rectangle('Position',[time(m) 0 1.5 0.8], ...
+              'LineWidth',2, ...
+              'FaceColor',[1 0 0])
+    end    
+end
+
+xlim([min(time) max(time)+1.5])
+end
