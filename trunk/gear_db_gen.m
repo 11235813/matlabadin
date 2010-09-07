d916b24c37b673f4c94edbec2dafb1a1d9367ce3
bentglasstube <bentglasstube@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-09-07 19:08:21 +0000

    new gear found in beta (333s are the 200s of cata)

---
+++ trunk/gear_db_gen.m
@@ -0,0 +1,369 @@
+idb.iid(51265).name='Sanctified Lightsworn Chestguard (Heroic)';
+idb.iid(51265).ilvl=277;
+idb.iid(51265).str=183;
+idb.iid(51265).sta=251;
+idb.iid(51265).dodge=56;
+idb.iid(51265).parry=96;
+idb.iid(51265).barmor=2756;
+idb.iid(51265).earmor=384;
+idb.iid(51265).rsock=1;
+idb.iid(51265).bsock=1;
+idb.iid(51265).slot=5;
+idb.iid(51265).atype=1;
+idb.iid(51265).sb.sta=9;
+idb.iid(51265).sb.active=[1 0 1];
+
+idb.iid(51266).name='Sanctified Lightsworn Faceguard (Heroic)';
+idb.iid(51266).ilvl=277;
+idb.iid(51266).str=183;
+idb.iid(51266).sta=243;
+idb.iid(51266).dodge=128;
+idb.iid(51266).parry=88;
+idb.iid(51266).barmor=2239;
+idb.iid(51266).msock=1;
+idb.iid(51266).rsock=1;
+idb.iid(51266).slot=1;
+idb.iid(51266).atype=1;
+idb.iid(51266).sb.sta=12;
+idb.iid(51266).sb.active=[1 0 0];
+
+idb.iid(51267).name='Sanctified Lightsworn Handguards (Heroic)';
+idb.iid(51267).ilvl=277;
+idb.iid(51267).str=103;
+idb.iid(51267).sta=192;
+idb.iid(51267).hit=69;
+idb.iid(51267).dodge=68;
+idb.iid(51267).barmor=1723;
+idb.iid(51267).earmor=328;
+idb.iid(51267).ysock=1;
+idb.iid(51267).atype=1;
+idb.iid(51267).sb.sta=6;
+idb.iid(51267).sb.active=[0 1 0];
+
+idb.iid(51268).name='Sanctified Lightsworn Legguards (Heroic)';
+idb.iid(51268).ilvl=277;
+idb.iid(51268).str=139;
+idb.iid(51268).sta=251;
+idb.iid(51268).exp=93;
+idb.iid(51268).dodge=167;
+idb.iid(51268).barmor=2412;
+idb.iid(51268).bsock=1;
+idb.iid(51268).ysock=1;
+idb.iid(51268).slot=9;
+idb.iid(51268).atype=1;
+idb.iid(51268).sb.sta=9;
+idb.iid(51268).sb.active=[0 1 1];
+
+idb.iid(51269).name='Sanctified Lightsworn Shoulderguards (Heroic)';
+idb.iid(51269).ilvl=277;
+idb.iid(51269).str=136;
+idb.iid(51269).sta=192;
+idb.iid(51269).dodge=99;
+idb.iid(51269).parry=71;
+idb.iid(51269).barmor=2067;
+idb.iid(51269).rsock=1;
+idb.iid(51269).atype=1;
+idb.iid(51269).sb.sta=6;
+idb.iid(51269).sb.active=[1 0 0];
+
+idb.iid(50682).name='Bile-Encrusted Medallion (Heroic)';
+idb.iid(50682).ilvl=277;
+idb.iid(50682).str=102;
+idb.iid(50682).sta=141;
+idb.iid(50682).dodge=73;
+idb.iid(50682).earmor=216;
+idb.iid(50682).rsock=1;
+idb.iid(50682).slot=2;
+idb.iid(50682).sb.sta=6;
+idb.iid(50682).sb.active=[1 0 0];
+
+idb.iid(50466).name='Sentinel''s Winter Cloak';
+idb.iid(50466).ilvl=264;
+idb.iid(50466).str=90;
+idb.iid(50466).sta=124;
+idb.iid(50466).dodge=72;
+idb.iid(50466).barmor=556;
+idb.iid(50466).earmor=160;
+idb.iid(50466).ysock=1;
+idb.iid(50466).slot=4;
+idb.iid(50466).sb.sta=6;
+idb.iid(50466).sb.active=[0 1 0];
+
+idb.iid(50991).name='Verdigris Chain Belt';
+idb.iid(50991).ilvl=264;
+idb.iid(50991).str=120;
+idb.iid(50991).sta=157;
+idb.iid(50991).parry=95;
+idb.iid(50991).barmor=1486;
+idb.iid(50991).earmor=188;
+idb.iid(50991).rsock=1;
+idb.iid(50991).bsock=1;
+idb.iid(50991).slot=8;
+idb.iid(50991).atype=1;
+idb.iid(50991).sb.sta=9;
+idb.iid(50991).sb.active=[1 0 1];
+
+idb.iid(50625).name='Grinning Skull Greatboots (Heroic)';
+idb.iid(50625).ilvl=277;
+idb.iid(50625).str=103;
+idb.iid(50625).sta=180;
+idb.iid(50625).exp=61;
+idb.iid(50625).dodge=127;
+idb.iid(50625).barmor=1895;
+idb.iid(50625).bsock=1;
+idb.iid(50625).ysock=1;
+idb.iid(50625).slot=10;
+idb.iid(50625).atype=1;
+idb.iid(50625).sb.sta=9;
+idb.iid(50625).sb.active=[0 1 1];
+
+idb.iid(50622).name='Devium''s Eternally Cold Ring (Heroic)';
+idb.iid(50622).ilvl=277;
+idb.iid(50622).str=102;
+idb.iid(50622).sta=141;
+idb.iid(50622).dodge=73;
+idb.iid(50622).earmor=216;
+idb.iid(50622).rsock=1;
+idb.iid(50622).slot=11;
+idb.iid(50622).sb.sta=6;
+idb.iid(50622).sb.active=[1 0 0];
+
+idb.iid(50404).name='Ashen Band of Endless Courage';
+idb.iid(50404).ilvl=277;
+idb.iid(50404).str=68;
+idb.iid(50404).sta=130;
+idb.iid(50404).hit=55;
+idb.iid(50404).dodge=84;
+idb.iid(50404).ysock=1;
+idb.iid(50404).slot=11;
+idb.iid(50404).sb.sta=6;
+idb.iid(50404).sb.active=[0 1 0];
+
+idb.iid(50356).name='Corroded Skeleton Key';
+idb.iid(50356).ilvl=264;
+idb.iid(50356).sta=228;
+idb.iid(50356).slot=13;
+
+idb.iid(54571).name='Petrified Twilight Scale';
+idb.iid(54571).ilvl=271;
+idb.iid(54571).slot=13;
+
+idb.iid(50708).name='Last Word (Heroic)';
+idb.iid(50708).wtype='mac';
+idb.iid(50708).ilvl=277;
+idb.iid(50708).tooldps=250.6;
+idb.iid(50708).swing=1.8;
+idb.iid(50708).avgdmg=451;
+idb.iid(50708).sta=94;
+idb.iid(50708).ysock=1;
+idb.iid(50708).slot=15;
+idb.iid(50708).sb.sta=6;
+idb.iid(50708).sb.active=[0 1 0];
+
+idb.iid(50729).name='Icecrown Glacial Wall (Heroic)';
+idb.iid(50729).ilvl=277;
+idb.iid(50729).str=102;
+idb.iid(50729).sta=141;
+idb.iid(50729).dodge=68;
+idb.iid(50729).parry=72;
+idb.iid(50729).barmor=7699;
+idb.iid(50729).rsock=1;
+idb.iid(50729).slot=16;
+idb.iid(50729).sb.sta=6;
+idb.iid(50729).sb.active=[1 0 0];
+
+idb.iid(50461).name='Libram of the Eternal Tower';
+idb.iid(50461).ilvl=264;
+idb.iid(50461).dodge=219;
+idb.iid(50461).slot=17;
+
+idb.iid(55059).name='Hardened Elementium Girdle';
+idb.iid(55059).ilvl=359;
+idb.iid(55059).str=253;
+idb.iid(55059).sta=380;
+idb.iid(55059).mast=169;
+idb.iid(55059).dodge=169;
+idb.iid(55059).barmor=1927;
+idb.iid(55059).slot=8;
+idb.iid(55059).atype=1;
+
+idb.iid(55058).name='Hardened Elementium Hauberk';
+idb.iid(55058).ilvl=359;
+idb.iid(55058).str=341;
+idb.iid(55058).sta=512;
+idb.iid(55058).exp=228;
+idb.iid(55058).parry=228;
+idb.iid(55058).barmor=3426;
+idb.iid(55058).slot=5;
+idb.iid(55058).atype=1;
+
+idb.iid(59359).name='Reinforced Bio-Optic Killshades';
+idb.iid(59359).ilvl=359;
+idb.iid(59359).str=341;
+idb.iid(59359).sta=512;
+idb.iid(59359).barmor=2784;
+idb.iid(59359).msock=1;
+idb.iid(59359).slot=1;
+idb.iid(59359).atype=1;
+idb.iid(59359).sb.str=10;
+idb.iid(59359).sb.active=[0 0 0];
+
+idb.iid(57268).name='Forgemaster''s Shattered Shackle';
+idb.iid(57268).ilvl=288;
+idb.iid(57268).str=131;
+idb.iid(57268).sta=196;
+idb.iid(57268).exp=92;
+idb.iid(57268).dodge=79;
+idb.iid(57268).barmor=1465;
+idb.iid(57268).slot=8;
+idb.iid(57268).atype=1;
+
+idb.iid(55023).name='Hardened Obsidian Helm';
+idb.iid(55023).ilvl=298;
+idb.iid(55023).str=194;
+idb.iid(55023).sta=290;
+idb.iid(55023).hit=129;
+idb.iid(55023).parry=129;
+idb.iid(55023).barmor=2204;
+idb.iid(55023).slot=1;
+idb.iid(55023).atype=1;
+
+idb.iid(54876).name='Hardened Obsidian Shoulders';
+idb.iid(54876).ilvl=298;
+idb.iid(54876).str=144;
+idb.iid(54876).sta=215;
+idb.iid(54876).exp=96;
+idb.iid(54876).dodge=96;
+idb.iid(54876).barmor=2034;
+idb.iid(54876).atype=1;
+
+idb.iid(59785).name='Pounded Pewter Links';
+idb.iid(59785).ilvl=288;
+idb.iid(59785).str=131;
+idb.iid(59785).sta=196;
+idb.iid(59785).dodge=77;
+idb.iid(59785).parry=94;
+idb.iid(59785).barmor=1465;
+idb.iid(59785).slot=8;
+idb.iid(59785).atype=1;
+
+idb.iid(58483).name='Lifebound Alchemist Stone';
+idb.iid(58483).ilvl=359;
+idb.iid(58483).sta=299;
+idb.iid(58483).mast=233;
+idb.iid(58483).bsock=1;
+idb.iid(58483).slot=13;
+
+idb.iid(52352).name='Figurine - Earthen Guardian';
+idb.iid(52352).ilvl=346;
+idb.iid(52352).sta=197;
+idb.iid(52352).slot=13;
+
+idb.iid(59607).name='Hallazeal''s Demise';
+idb.iid(59607).ilvl=288;
+idb.iid(59607).str=98;
+idb.iid(59607).sta=148;
+idb.iid(59607).exp=58;
+idb.iid(59607).dodge=70;
+idb.iid(59607).slot=11;
+
+idb.iid(56549).name='Twilight Dragonscale Cloak';
+idb.iid(56549).ilvl=359;
+idb.iid(56549).str=190;
+idb.iid(56549).sta=286;
+idb.iid(56549).mast=145;
+idb.iid(56549).dodge=96;
+idb.iid(56549).barmor=625;
+idb.iid(56549).slot=4;
+
+idb.iid(61402).name='Riftrent Waraxe';
+idb.iid(61402).wtype='axe';
+idb.iid(61402).ilvl=318;
+idb.iid(61402).tooldps=315.4;
+idb.iid(61402).swing=2.6;
+idb.iid(61402).avgdmg=820;
+idb.iid(61402).str=100;
+idb.iid(61402).sta=150;
+idb.iid(61402).hit=67;
+idb.iid(61402).mast=67;
+idb.iid(61402).slot=15;
+
+idb.iid(56130).name='Mace of Transformed Bone';
+idb.iid(56130).wtype='mac';
+idb.iid(56130).ilvl=333;
+idb.iid(56130).tooldps=362.7;
+idb.iid(56130).swing=2.6;
+idb.iid(56130).avgdmg=943;
+idb.iid(56130).str=87;
+idb.iid(56130).sta=172;
+idb.iid(56130).hit=58;
+idb.iid(56130).dodge=115;
+idb.iid(56130).slot=15;
+
+idb.iid(56096).name='Bulwark of the Primordial Mound';
+idb.iid(56096).ilvl=333;
+idb.iid(56096).str=114;
+idb.iid(56096).sta=224;
+idb.iid(56096).hit=76;
+idb.iid(56096).dodge=149;
+idb.iid(56096).barmor=11504;
+idb.iid(56096).slot=16;
+
+idb.iid(56099).name='Fingers of Light';
+idb.iid(56099).ilvl=333;
+idb.iid(56099).str=199;
+idb.iid(56099).sta=298;
+idb.iid(56099).mast=133;
+idb.iid(56099).parry=133;
+idb.iid(56099).barmor=1955;
+idb.iid(56099).atype=1;
+
+idb.iid(56124).name='Earthshape Pauldrons';
+idb.iid(56124).ilvl=333;
+idb.iid(56124).str=199;
+idb.iid(56124).sta=298;
+idb.iid(56124).mast=133;
+idb.iid(56124).dodge=133;
+idb.iid(56124).barmor=2346;
+idb.iid(56124).atype=1;
+
+idb.iid(55992).name='Armguards of Unearthly Light';
+idb.iid(55992).ilvl=333;
+idb.iid(55992).str=114;
+idb.iid(55992).sta=224;
+idb.iid(55992).hit=76;
+idb.iid(55992).dodge=149;
+idb.iid(55992).barmor=1369;
+idb.iid(55992).slot=6;
+idb.iid(55992).atype=1;
+
+idb.iid(56219).name='Shroud of Dark Memories';
+idb.iid(56219).ilvl=333;
+idb.iid(56219).str=114;
+idb.iid(56219).sta=224;
+idb.iid(56219).exp=76;
+idb.iid(56219).dodge=149;
+idb.iid(56219).barmor=572;
+idb.iid(56219).slot=4;
+
+idb.iid(56111).name='Temple Band';
+idb.iid(56111).ilvl=333;
+idb.iid(56111).str=149;
+idb.iid(56111).sta=224;
+idb.iid(56111).mast=100;
+idb.iid(56111).dodge=100;
+idb.iid(56111).slot=11;
+
+idb.iid(55888).name='Darkhowl Amulet';
+idb.iid(55888).ilvl=333;
+idb.iid(55888).str=114;
+idb.iid(55888).sta=224;
+idb.iid(55888).exp=76;
+idb.iid(55888).dodge=149;
+idb.iid(55888).slot=2;
+
+idb.iid(56121).name='Throngus''s Finger';
+idb.iid(56121).ilvl=333;
+idb.iid(56121).dodge=252;
+idb.iid(56121).slot=13;
+
