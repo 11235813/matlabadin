112b533ed0c148b23505d93d00f8044d7c19e36e
tlitp0 <tlitp0@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-10-05 12:04:51 +0000

    - minor fixes

--- trunk/stat_model.m
+++ trunk/stat_model.m
@@ -330,24 +330,18 @@ player.block=base.block+16.*(talent.protpoints>=30)+mdf.HolySh ...
     +gear.block./cnv.block_block+2.*player.mast-0.04.*npc.skillgap;
 
 %check for bounding issues, based on the attack table
-%separated into two stages so that it works with arrays more easily.
-%First, enforce minimum of 0
 player.miss=max([player.miss;zeros(size(player.miss))]);
 player.dodge=max([player.dodge;zeros(size(player.dodge))]);
 player.parry=max([player.parry;zeros(size(player.parry))]);
 player.block=max([player.block;zeros(size(player.block))]);
 
-%next, enforce maxima
-avsize=max([size(player.miss); size(player.parry); size(player.dodge); size(player.block)]);
-
-player.dodge=min([player.dodge.*ones(avsize); ...
-    (100-player.miss).*ones(avsize)]);
-player.parry=min([player.parry.*ones(avsize); ...
-    (100-player.miss-player.dodge).*ones(avsize)]);
-player.block=min([player.block.*ones(avsize); ...
-    (100-player.miss-player.dodge-player.parry).*ones(avsize)]);
-
-
+player.avsize=max([size(player.miss);size(player.parry);size(player.dodge);size(player.block)]);
+player.dodge=min([player.dodge.*ones(player.avsize); ...
+    (100-player.miss).*ones(player.avsize)]);
+player.parry=min([player.parry.*ones(player.avsize); ...
+    (100-player.miss-player.dodge).*ones(player.avsize)]);
+player.block=min([player.block.*ones(player.avsize); ...
+    (100-player.miss-player.dodge-player.parry).*ones(player.avsize)]);
 
 player.avoid=player.miss+player.dodge+player.parry;
 player.avoidpct=player.avoid./100;
