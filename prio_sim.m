0560667fcd1ff66c624cf18e94da5a9dfae264b5
theckhd <theckhd@gmail.com>
2010-10-08 19:02:45 +0000

    Significant rewrite of prio_* for speed and ease of use changes

--- prio_sim.m
+++ prio_sim.m
@@ -9,7 +9,7 @@ function [ sequence ] = prio_sim(pri,varargin)
 
 %import priority queue models, cooldowns, and modifiers
 prio=evalin('base','prio');
-cd=evalin('base','cd');
+% cd=evalin('base','cd');
 mdf=evalin('base','mdf');
 target=evalin('base','target');  %needed for ShoR
 %the rest are all needed for damage calculations
@@ -20,6 +20,7 @@ target=evalin('base','target');  %needed for ShoR
 % gear=evalin('base','gear');
 % exec=evalin('base','exec');
 % npc=evalin('base','npc');
+
 
 
 %if no arguments
@@ -68,10 +69,20 @@ clear t sequence
 gcd=0;
 egcd=0;
 
+%import cooldowns from prio
+% cd=pri.cds;
+cd.CS=pri.cds(2);
+cd.Jud=pri.cds(3);
+cd.AS=pri.cds(4);
+cd.HW=pri.cds(5);
+cd.Cons=pri.cds(6);
+
 %set "current cooldown" for all relevant spells 
+% ccd=zeros(size(cd));
+
 ccd.CS=0;
 ccd.AS=0;
-ccd.HoWr=0;
+ccd.HW=0;
 ccd.Jud=0;
 ccd.Cons=0;
 
@@ -85,9 +96,16 @@ for m=1:length(pri.setup)
     eval(char(pri.setup(m)));
 end
 
-% damage=zeros(N,1);
-% spell=cell(N);
-% label=cell(N);
+%preallocate arrays for speed
+L=round(N.*dt./1.5);
+sequence.amatrix=zeros(length(pri.alabel),L);
+sequence.pmatrix=zeros(length(pri.plabel),L);
+sequence.castid=zeros(1,L);
+sequence.SD=zeros(1,L);
+sequence.Inq=zeros(1,L);
+sequence.hopo=zeros(1,L);
+sequence.shormiss=zeros(1,L);
+t=zeros(1,N);
 
 %counter for spell casts
 qq=1;
@@ -107,7 +125,7 @@ for m=1:N
         %evaluate priority conditionals
         for n=1:length(pri.cond)
 
-            %check each conditional in order
+            %check each cooldown and conditional in order
             if eval(char(pri.cond(n)))
                 %if true, do stuff
                 
@@ -118,53 +136,58 @@ for m=1:N
                 %   -what was cast 
                 %   -the timestamp of the cast (for plotting)
                 %   -Status of buffs (SD & Inq)
-                %   -Hit/Miss for ShoR (so that we can properly evaluate damage later)
-                
+                %   -any procs that should occur
+                %   -Hit/Miss for ShoR (so that we can properly evaluate damage later)                
                 %the sequence structure will track all of these
+                
+                %ability usage and proc triggers
+                sequence.amatrix(aid,qq)=1;
+                sequence.label{qq}=char(pri.alabel(aid));                
                 sequence.castid(qq)=aid;
+                
+                sequence.pmatrix(:,qq)=pri.ptrig(:,aid);
+                
+                %cast time
                 sequence.casttime(qq)=t(m);
+                
+                %SD/Inq duration
                 sequence.SD(qq)=dur.SD;
                 sequence.Inq(qq)=dur.Inq;
+                
+                %holy power
                 sequence.hopo(qq)=hopo;
-                sequence.spellname{qq}=char(pri.castname(aid));
-                sequence.label{qq}=char(pri.labels(aid));
-                sequence.seq(qq)=pri.ids(aid);
-                sequence.color(qq)=0; %placeholder for later
                 
+                %placeholders for later - related to rotation plots
+%                 sequence.seq(qq)=pri.ids(aid);
+%                 sequence.color(qq)=0; 
                 
-                
-                %i want to handle damage after-the-fact, but I'll leave
-                %this here for now in case we decide it's relevant.
-%                 sequence.damage(qq)=pri.dmg(aid);
-            
-                
-                %perform actions
-                eval(char(pri.action(aid)));
-                
+                               
                 %ShoR handling
-                if strcmp(char(pri.castname(aid)),'ShieldoftheRighteous')
+                if sum(strcmp(char(pri.alabel(aid)),{'3ShoR';'2ShoR';'1ShoR'}))>0
                     %check for misses
                     if rand<target.avoid/100
-                        %if we miss, set the flag and color 
+                        %if we miss, set the flag and color and nullify
+                        %procs
                         sequence.shormiss(qq)=1;
-                        sequence.color(qq)=2;
-                        %sacred duty cleared at end of if statement
+                        sequence.pmatrix(:,qq)=zeros(size(sequence.pmatrix(:,qq)));
+%                         sequence.color(qq)=2;
 
                     elseif dur.SD>0  %otherwise, we hit - check for SD crits
-                        sequence.color(qq)=1;
+%                         sequence.color(qq)=1;
                         sequence.shormiss(qq)=0;
 
                     else %no SD, just a regular hit
                         sequence.shormiss(qq)=0;
                     end
-
-                    %clear SD, regardless of outcome
+                      
                     dur.SD=0;
-
-                else %not ShoR, mark as not a miss (irrelevant really)
+                else %not shor, mark as a miss (irrelevant, but necessary if we guessed array lengths incorrectly)
                     sequence.shormiss(qq)=0;
                 end
 
+                %perform actions
+                eval(char(pri.action(aid)));
+                
                 %special actions (performed last)
                 eval(char(pri.spaction(n)));
 
@@ -186,27 +209,32 @@ for m=1:N
     %variable to limit it
     if gcd<=0 && egcd<=0
         
-%        sequence.damage(qq)=0;
+%        sequence.spellname{qq}='Empty';
        sequence.castid(qq)=0;
        sequence.casttime(qq)=t(m);
        sequence.SD(qq)=dur.SD;
        sequence.Inq(qq)=dur.Inq;
        sequence.hopo(qq)=hopo;
-       sequence.spellname{qq}='Empty';
        sequence.label{qq}='Empty';
-       sequence.seq(qq)=0;
-       sequence.color(qq)=2;
+%        sequence.seq(qq)=0;
+%        sequence.color(qq)=2;
        sequence.shormiss(qq)=0;
+       
+       %necessary to make sure amatrix and pmatrix are the correct length
+       %in some cases
+       sequence.amatrix(1,qq)=0;
+       sequence.pmatrix(1,qq)=0;
        
        qq=qq+1;
        egcd=1.5; 
     end
     
-    %reduce cooldowns   TODO: Implement generic ccd array
+    %reduce cooldowns 
+%     ccd=ccd-dt;
     ccd.CS=ccd.CS-dt;
     ccd.AS=ccd.AS-dt; 
     ccd.Jud=ccd.Jud-dt;
-    ccd.HoWr=ccd.HoWr-dt;
+    ccd.HW=ccd.HW-dt;
     ccd.Cons=ccd.Cons-dt;
     gcd=gcd-dt;
     egcd=egcd-dt;
@@ -215,13 +243,6 @@ for m=1:N
     dur.SD=max([dur.SD-dt 0]);
     dur.Inq=max([dur.Inq-dt 0]);
     
-    %modifiers
-%     mdf.Inq=1.3.*sign(dur.Inq);
-%     mdf.SDcrit=1.*sign(dur.SD);
-    
-    %recalculate ability damages - probably not necessary if we're not
-    %going to track damages locally
-%     ability_model;
     
     %kludgy fix for numerical errors - 
     %after subtraction @ dt=0.1, AS sits at 3.6554e-14.  Depending on dt,
@@ -231,10 +252,12 @@ for m=1:N
     %dt=0.0001.  This is a patch-up job based on roundn() which is in the
     %mapping toolbox, and thus not necessarily available to Octave.
     factor=1e10;
+%     ccd=round(ccd.*factor)./factor;
     ccd.CS=round(ccd.CS.*factor)./factor;
     ccd.AS=round(ccd.AS.*factor)./factor;
     ccd.Jud=round(ccd.Jud.*factor)./factor;
-    ccd.HoWr=round(ccd.HoWr.*factor)./factor;
+    ccd.HW=round(ccd.HW.*factor)./factor;
+    ccd.Cons=round(ccd.Cons.*factor)./factor;
     dur.SD=round(dur.SD.*factor)./factor;dur.SD=max([dur.SD 0]);
     gcd=round(gcd.*factor)./factor;
     egcd=round(egcd.*factor)./factor;
@@ -245,45 +268,25 @@ end
 
 %determine weighting coefficients for each spell
 %Helpful constants                   
-Inqmod=(0.3.*(sequence.Inq>0));
-sequence.totaltime=m*dt;
+Iamod=(1+0.3.*pri.adtype'*(sequence.Inq>0));
+Ipmod=(1+0.3.*pri.pdtype'*(sequence.Inq>0));
+sequence.totaltime=double(m*dt);
+sequence.eamatrix=sequence.amatrix.*Iamod;
+sequence.epmatrix=ones(2,1)*sum(pri.phit'*ones(1,size(sequence.amatrix,2)).*sequence.amatrix).*sequence.pmatrix;
 
-for mm=1:length(pri.labels)
-    %if we're evaluating a ShoR
-    if sum(strcmp(char(pri.labels{mm}),{'3ShoR';'2ShoR';'1ShoR'}))>0
-        sequence.shorflag(mm)=1;
-        sequence.numcasts(mm)=sum((sequence.castid==mm).*not(sequence.shormiss)); 
-        sequence.effcasts(mm)=sum((1+Inqmod).* ...  %Inq handling
-            ((sequence.castid==mm).*not(sequence.shormiss).*(sequence.SD==0).*mdf.phcrit+... %# ShoR hits/crits
-            (sequence.castid==mm).*not(sequence.shormiss).*(sequence.SD>0).*mdf.phcritmulti) ... %# ShoR SD crits
-            ); 
-        sequence.sealcasts(mm)=sum((1+Inqmod).* ...  %Inq handling
-            ((sequence.castid==mm).*not(sequence.shormiss).*(sequence.SD==0)+... %# ShoR hits/crits
-            (sequence.castid==mm).*not(sequence.shormiss).*(sequence.SD>0)) ... %# ShoR SD crits
-            ); 
-        
-    elseif strcmp(char(pri.labels{mm}),'HaNova')==1
-        %HotR base is taken care of in 'everything else'
-        %HammerNova gets counted every time HotR is cast
-        sequence.shorflag(mm)=0;
-        tempmm=find(strcmp(pri.castname,'HammeroftheRighteous'));
-        sequence.effcasts(mm)=sum((sequence.castid==tempmm).*(1+Inqmod.*pri.inqeffect(mm)));
-        sequence.numcasts(mm)=sum((sequence.castid==tempmm));
-        sequence.sealcasts(mm)=0; %does not proc seals
-    
-    else %everything else is easier - just Inq handling to worry about
-        sequence.shorflag(mm)=0;
-        sequence.effcasts(mm)=sum((sequence.castid==mm).*(1+Inqmod.*pri.inqeffect(mm)));
-        sequence.numcasts(mm)=sum((sequence.castid==mm));
-        sequence.sealcasts(mm)=pri.procsseals(mm).*pri.sealhit(mm).*sequence.effcasts(mm);
-    end
-end
+%fix for ShoR crit handling
+ShoRmod=(sequence.SD>0).*mdf.phcritmulti + (sequence.SD==0).*mdf.phcrit;
+sequence.eamatrix(1,:)=sequence.eamatrix(1,:).*not(sequence.shormiss).*ShoRmod;
 
-%seals 
-sequence.effcasts(mm+1)=sum(sequence.sealcasts);
+sequence.numcasts=sum([sequence.amatrix; sequence.pmatrix]')';
+sequence.effcasts=sum([sequence.eamatrix; sequence.epmatrix]')';
+
 
 sequence.coeff=sequence.effcasts./sequence.totaltime;
 sequence.empties=sum(sequence.castid==0);
+temp=find(sequence.castid(1:length(sequence.castid)-1)==0); %all but last one
+sequence.emptytime=sum(sequence.casttime(temp+1)-sequence.casttime(temp));
+sequence.emptypct=100.*sequence.emptytime./sequence.totaltime;
 
 % %"passive" dps (melee+seals+censure) - in case I decide to move this here
 % 
@@ -300,13 +303,13 @@ sequence.empties=sum(sequence.castid==0);
 
 %for the moment, only worry about this if damage values are scalars
 %later on we might 
-if min(size(pri.damage))==1
+if min(size(pri.admg))==1
 
-    sequence.dmg=[pri.damage; pri.sealdamage]'.*sequence.effcasts;
-    sequence.dps=[pri.damage; pri.sealdamage]'.*sequence.coeff;
-    sequence.net=pri.damage'.*sequence.coeff(1:mm)+pri.sealdamage.*sequence.sealcasts./sequence.totaltime;
-    % sequence.sumdps=sum(sequence.dps)+sequence.padps;
-    sequence.sumdps=sum(sequence.dps);
+    sequence.dmgbysrc=[pri.admg; pri.pdmg].*sequence.effcasts;
+    sequence.dpsbysrc=[pri.admg; pri.pdmg].*sequence.coeff;
+
+    sequence.dmg=sum(sequence.dmgbysrc);
+    sequence.dps=sum(sequence.dpsbysrc);
 
 end
 
