6ff7f37b6dde65de612195b4eb5ddaf1ecebceb1
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-10-12 02:26:36 +0000

    more updates

--- calc_talents.m
+++ calc_talents.m
@@ -10,7 +10,7 @@ def_db;
 base=player_model('lvl',80,'race','Human','prof','');
 npc=npc_model(base);
 gear_sample
-exec=execution_model('npccount',1,'timein',0,'timeout',1,'seal','Truth');
+exec=execution_model('npccount',1,'timein',1,'timeout',1,'seal','Truth');
 buff=buff_model('mode',1);
 
 
@@ -52,20 +52,6 @@ tree(k).prot(4,1)=0;
 points(k)=tree(1).prot(4,1);
 name{k}='Reck';
 
-%Grand Crusader
-k=k+1;
-tree(k)=tree(1);
-tree(k).prot(4,3)=0;
-points(k)=tree(1).prot(4,3);
-name{k}='Grand Crusader';
-
-%Sacred duty
-k=k+1;
-tree(k)=tree(1);
-tree(k).prot(6,2)=0;
-points(k)=tree(1).prot(6,2);
-name{k}='Sacred Duty';
-
 %Arbiter of the Light
 k=k+1;
 tree(k)=tree(1);
@@ -94,14 +80,63 @@ tree(k).ret(2,2)=0;
 points(k)=tree(1).ret(2,2);
 name{k}='RoL';
 
+%Grand Crusader
+k=k+1;
+tree(k)=tree(1);
+tree(k).prot(4,3)=0;
+points(k)=tree(1).prot(4,3);
+name{k}='Grand Crusader';
+
+%Sacred duty
+k=k+1;
+tree(k)=tree(1);
+tree(k).prot(6,2)=0;
+points(k)=tree(1).prot(6,2);
+name{k}='Sacred Duty';
 
 
+%% sim 
 
+%Calculate a sequence for this build, we'll use this sequence for all of
+%the talents that don't have a direct effect on the rotation 
+%(i.e. all but GC).
+clear talent
+talent=tree(1);
+%invoke talents & glyphs
+talents
+%calculate relevant stats
+gear_stats
+%calculate final stats
+stat_model
+ability_model_80
+prio_model
 
-%% sim
-wb=waitbar(0,'Calculating');
-for m=1:length(tree)
-    
+%run sim
+seq1=prio_sim(10,'N',90000);
+
+%active DPS
+acdps(1)=sum(pridmg.*seq1.coeff);
+      
+%calculate passive damage sources
+padps(1)=0;
+%account for Inq
+Inqmod=sum(seq1.Inq>0)./length(seq1.Inq);
+
+%assume a 5-stack of SoT (if applicable).
+if strcmpi('Truth',exec.seal)||strcmpi('SoT',exec.seal)
+    padps(1)=padps(1)+dps.Censure.*(1+0.3.*Inqmod);
+
+end
+
+%aa and seal damage
+padps(1)=padps(1)+dps.Melee+dmg.activeseal.*mdf.mehit.*(1+0.3.*Inqmod)./player.wswing;
+
+%now calculate total DPS
+totdps(1)=acdps(1)+padps(1);
+
+for m=2:length(tree)-2  %everything except GC & SD
+   
+
     clear talent
     talent=tree(m);
     %invoke talents & glyphs
@@ -111,15 +146,12 @@ for m=1:length(tree)
     %calculate final stats
     stat_model
     ability_model_80
-    prio_model
-
-    seq(m)=prio_sim(8,'N',60000);
-    waitbar(m/length(tree),wb)
-
-
+    
+    %active dps
+    acdps(m)=sum(pridmg.*seq1.coeff);
     padps(m)=0;
     %account for Inq
-    Inqmod=sum(seq(m).Inq>0)./length(seq(m).Inq);
+    Inqmod=sum(seq1.Inq>0)./length(seq1.Inq);
 
     %assume a 5-stack of SoT (if applicable).
     if strcmpi('Truth',exec.seal)||strcmpi('SoT',exec.seal)
@@ -130,9 +162,54 @@ for m=1:length(tree)
     %aa and seal damage
     padps(m)=padps(m)+dps.Melee+dmg.activeseal.*mdf.mehit.*(1+0.3.*Inqmod)./player.wswing;
 
-    totdps(m)=seq(m).sumdps+padps(m);
+    totdps(m)=acdps(m)+padps(m);
+end
+
+
+%% Grand Crusader & Sacred Duty
+%These are more annoying, we need to sim out another version with GC
+%inactive and compare DPS.  This is a bit of a pain since GC can come out
+%negative if we're unlucky. 
+
+%TODO: fix prio_sim to make it possible to do Sacred Duty without
+%re-simming
+
+wb=waitbar(0,'Calculating GC');
+for m=length(tree)-1:length(tree)
+    
+    clear talent
+    talent=tree(m);
+    %invoke talents & glyphs
+    talents
+    %calculate relevant stats
+    gear_stats
+    %calculate final stats
+    stat_model
+    ability_model_80
+
+    seq2(m)=prio_sim(10,'N',90000);
+    waitbar(m/length(tree),wb)
+    
+    acdps(m)=sum(pridmg.*seq2(m).coeff)
+
+    padps(m)=0;
+    %account for Inq
+    Inqmod=sum(seq2(m).Inq>0)./length(seq2(m).Inq);
+
+    %assume a 5-stack of SoT (if applicable).
+    if strcmpi('Truth',exec.seal)||strcmpi('SoT',exec.seal)
+        padps(m)=padps(m)+dps.Censure.*(1+0.3.*Inqmod);
+
+    end
+
+    %aa and seal damage
+    padps(m)=padps(m)+dps.Melee+dmg.activeseal.*mdf.mehit.*(1+0.3.*Inqmod)./player.wswing;
+
+    totdps(m)=acdps(m)+padps(m);
 
 end
+
+
 close(wb)    
 
 [char(name) repmat(' ',length(name),5) num2str((totdps(1)-totdps')./points','%2.1f')]
