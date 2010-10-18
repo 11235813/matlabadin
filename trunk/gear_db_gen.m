fa04bc6c3e65747e7bcd443b83015a6aeb8a3dfb
bentglasstube <bentglasstube@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-10-18 19:19:52 +0000

    gear additions

--- trunk/gear_db_gen.m
+++ trunk/gear_db_gen.m
@@ -175,289 +175,9 @@ idb.iid(50461).name='Libram of the Eternal Tower';
 idb.iid(50461).ilvl=264;
 idb.iid(50461).str=51;
 idb.iid(50461).sta=77;
-idb.iid(50461).dodge=219;
+idb.iid(50461).dodge=34;
 idb.iid(50461).parry=34;
 idb.iid(50461).rsock=1;
-
-idb.iid(55059).name='Hardened Elementium Girdle';
-idb.iid(55059).ilvl=359;
-idb.iid(55059).str=253;
-idb.iid(55059).sta=380;
-idb.iid(55059).mast=169;
-idb.iid(55059).dodge=169;
-idb.iid(55059).barmor=1927;
-idb.iid(55059).atype=1;
-
-idb.iid(55058).name='Hardened Elementium Hauberk';
-idb.iid(55058).ilvl=359;
-idb.iid(55058).str=341;
-idb.iid(55058).sta=512;
-idb.iid(55058).exp=228;
-idb.iid(55058).parry=228;
-idb.iid(55058).barmor=3426;
-idb.iid(55058).atype=1;
-
-idb.iid(59359).name='Reinforced Bio-Optic Killshades';
-idb.iid(59359).ilvl=359;
-idb.iid(59359).str=341;
-idb.iid(59359).sta=512;
-idb.iid(59359).barmor=2784;
-idb.iid(59359).msock=1;
-idb.iid(59359).atype=1;
-idb.iid(59359).sb.str=10;
-idb.iid(59359).sb.active=[0 0 0];
-
-idb.iid(58483).name='Lifebound Alchemist Stone';
-idb.iid(58483).ilvl=359;
-idb.iid(58483).sta=380;
-idb.iid(58483).mast=213;
-idb.iid(58483).bsock=1;
-idb.iid(58483).sb.crit=10;
-idb.iid(58483).sb.active=[0 0 1];
-
-idb.iid(52352).name='Figurine - Earthen Guardian';
-idb.iid(52352).ilvl=346;
-idb.iid(52352).sta=197;
-
-idb.iid(56549).name='Twilight Dragonscale Cloak';
-idb.iid(56549).ilvl=359;
-idb.iid(56549).str=190;
-idb.iid(56549).sta=286;
-idb.iid(56549).mast=145;
-idb.iid(56549).dodge=96;
-idb.iid(56549).barmor=625;
-
-idb.iid(62351).name='Felsen''s Ring of Resolve';
-idb.iid(62351).ilvl=346;
-idb.iid(62351).str=168;
-idb.iid(62351).sta=252;
-idb.iid(62351).mast=112;
-idb.iid(62351).dodge=112;
-
-idb.iid(62432).name='Gryphon Rider''s Boots';
-idb.iid(62432).ilvl=359;
-idb.iid(62432).str=253;
-idb.iid(62432).sta=380;
-idb.iid(62432).mast=159;
-idb.iid(62432).parry=139;
-idb.iid(62432).barmor=2355;
-idb.iid(62432).ysock=1;
-idb.iid(62432).atype=1;
-idb.iid(62432).sb.parry=10;
-idb.iid(62432).sb.active=[0 1 0];
-
-idb.iid(62428).name='Crown of Wings';
-idb.iid(62428).ilvl=346;
-idb.iid(62428).str=196;
-idb.iid(62428).sta=454;
-idb.iid(62428).exp=137;
-idb.iid(62428).dodge=242;
-idb.iid(62428).barmor=2680;
-idb.iid(62428).msock=1;
-idb.iid(62428).rsock=1;
-idb.iid(62428).atype=1;
-idb.iid(62428).sb.dodge=30;
-idb.iid(62428).sb.active=[1 0 0];
-
-idb.iid(62449).name='Sandguard Bracers';
-idb.iid(62449).ilvl=359;
-idb.iid(62449).str=136;
-idb.iid(62449).sta=286;
-idb.iid(62449).exp=111;
-idb.iid(62449).dodge=190;
-idb.iid(62449).barmor=1499;
-idb.iid(62449).atype=1;
-
-idb.iid(62471).name='Mirror of Broken Images';
-idb.iid(62471).ilvl=359;
-idb.iid(62471).mast=321;
-
-idb.iid(57932).name='The Lustrous Eye';
-idb.iid(57932).ilvl=346;
-idb.iid(57932).str=168;
-idb.iid(57932).sta=252;
-idb.iid(57932).mast=112;
-idb.iid(57932).dodge=112;
-
-idb.iid(57914).name='Girdle of the Mountains';
-idb.iid(57914).ilvl=346;
-idb.iid(57914).str=225;
-idb.iid(57914).sta=337;
-idb.iid(57914).dodge=110;
-idb.iid(57914).parry=150;
-idb.iid(57914).barmor=1855;
-idb.iid(57914).rsock=1;
-idb.iid(57914).atype=1;
-idb.iid(57914).sb.sta=15;
-idb.iid(57914).sb.active=[1 0 0];
-
-idb.iid(58104).name='Sunburnt Pauldrons';
-idb.iid(58104).ilvl=346;
-idb.iid(58104).str=161;
-idb.iid(58104).sta=337;
-idb.iid(58104).hit=111;
-idb.iid(58104).dodge=205;
-idb.iid(58104).barmor=2474;
-idb.iid(58104).rsock=1;
-idb.iid(58104).atype=1;
-idb.iid(58104).sb.dodge=10;
-idb.iid(58104).sb.active=[1 0 0];
-
-idb.iid(58102).name='Greaves of Splendor';
-idb.iid(58102).ilvl=346;
-idb.iid(58102).str=210;
-idb.iid(58102).sta=454;
-idb.iid(58102).hit=133;
-idb.iid(58102).dodge=262;
-idb.iid(58102).barmor=2886;
-idb.iid(58102).ysock=1;
-idb.iid(58102).atype=1;
-idb.iid(58102).sb.sta=30;
-idb.iid(58102).sb.active=[0 1 0];
-
-idb.iid(58103).name='Helm of the Proud';
-idb.iid(58103).ilvl=346;
-idb.iid(58103).str=302;
-idb.iid(58103).sta=454;
-idb.iid(58103).dodge=142;
-idb.iid(58103).parry=142;
-idb.iid(58103).barmor=2680;
-idb.iid(58103).msock=1;
-idb.iid(58103).atype=1;
-idb.iid(58103).sb.sta=45;
-idb.iid(58103).sb.active=[0 0 0];
-
-idb.iid(58105).name='Numbing Handguards';
-idb.iid(58105).ilvl=346;
-idb.iid(58105).str=215;
-idb.iid(58105).sta=337;
-idb.iid(58105).mast=140;
-idb.iid(58105).dodge=130;
-idb.iid(58105).barmor=2061;
-idb.iid(58105).bsock=1;
-idb.iid(58105).atype=1;
-idb.iid(58105).sb.dodge=10;
-idb.iid(58105).sb.active=[0 0 1];
-
-idb.iid(58101).name='Chestplate of the Steadfast';
-idb.iid(58101).ilvl=346;
-idb.iid(58101).str=282;
-idb.iid(58101).sta=454;
-idb.iid(58101).mast=182;
-idb.iid(58101).dodge=162;
-idb.iid(58101).barmor=3298;
-idb.iid(58101).bsock=1;
-idb.iid(58101).ysock=1;
-idb.iid(58101).atype=1;
-idb.iid(58101).sb.dodge=20;
-idb.iid(58101).sb.active=[0 1 1];
-
-idb.iid(57926).name='Shield of the Four Grey Towers';
-idb.iid(57926).ilvl=346;
-idb.iid(57926).str=168;
-idb.iid(57926).sta=252;
-idb.iid(57926).mast=112;
-idb.iid(57926).dodge=112;
-idb.iid(57926).barmor=11696;
-
-idb.iid(60354).name='Reinforced Sapphirium Chestguard';
-idb.iid(60354).ilvl=359;
-idb.iid(60354).str=341;
-idb.iid(60354).sta=512;
-idb.iid(60354).mast=208;
-idb.iid(60354).parry=168;
-idb.iid(60354).barmor=3426;
-idb.iid(60354).rsock=1;
-idb.iid(60354).atype=1;
-idb.iid(60354).sb.sta=30;
-idb.iid(60354).sb.active=[1 0 0];
-
-idb.iid(60355).name='Reinforced Sapphirium Handguards';
-idb.iid(60355).ilvl=359;
-idb.iid(60355).str=193;
-idb.iid(60355).sta=380;
-idb.iid(60355).mast=149;
-idb.iid(60355).parry=149;
-idb.iid(60355).barmor=2141;
-idb.iid(60355).bsock=1;
-idb.iid(60355).atype=1;
-idb.iid(60355).sb.mast=10;
-idb.iid(60355).sb.active=[0 0 1];
-
-idb.iid(60356).name='Reinforced Sapphirium Faceguard';
-idb.iid(60356).ilvl=359;
-idb.iid(60356).str=259;
-idb.iid(60356).sta=512;
-idb.iid(60356).hit=113;
-idb.iid(60356).dodge=281;
-idb.iid(60356).barmor=2784;
-idb.iid(60356).msock=1;
-idb.iid(60356).atype=1;
-idb.iid(60356).sb.sta=45;
-idb.iid(60356).sb.active=[0 0 0];
-
-idb.iid(60357).name='Reinforced Sapphirium Legguards';
-idb.iid(60357).ilvl=359;
-idb.iid(60357).str=341;
-idb.iid(60357).sta=512;
-idb.iid(60357).dodge=188;
-idb.iid(60357).parry=188;
-idb.iid(60357).barmor=2998;
-idb.iid(60357).rsock=1;
-idb.iid(60357).atype=1;
-idb.iid(60357).sb.sta=30;
-idb.iid(60357).sb.active=[1 0 0];
-
-idb.iid(60358).name='Reinforced Sapphirium Shoulderguards';
-idb.iid(60358).ilvl=359;
-idb.iid(60358).str=193;
-idb.iid(60358).sta=380;
-idb.iid(60358).hit=128;
-idb.iid(60358).dodge=213;
-idb.iid(60358).barmor=2570;
-idb.iid(60358).rsock=1;
-idb.iid(60358).atype=1;
-idb.iid(60358).sb.sta=15;
-idb.iid(60358).sb.active=[1 0 0];
-
-idb.iid(64676).name='Relic of Khaz''goroth';
-idb.iid(64676).ilvl=359;
-idb.iid(64676).str=107;
-idb.iid(64676).sta=161;
-idb.iid(64676).mast=72;
-idb.iid(64676).dodge=72;
-idb.iid(64676).rsock=1;
-
-idb.iid(58182).name='Bedrock Talisman';
-idb.iid(58182).ilvl=359;
-idb.iid(58182).sta=482;
-
-idb.iid(58187).name='Ring of the Battle Anthem';
-idb.iid(58187).ilvl=359;
-idb.iid(58187).str=190;
-idb.iid(58187).sta=286;
-idb.iid(58187).mast=127;
-idb.iid(58187).dodge=127;
-
-idb.iid(58192).name='Gray Hair Cloak';
-idb.iid(58192).ilvl=359;
-idb.iid(58192).str=136;
-idb.iid(58192).sta=286;
-idb.iid(58192).exp=111;
-idb.iid(58192).dodge=190;
-idb.iid(58192).barmor=625;
-
-idb.iid(58197).name='Rock Furrow Boots';
-idb.iid(58197).ilvl=359;
-idb.iid(58197).str=181;
-idb.iid(58197).sta=380;
-idb.iid(58197).exp=138;
-idb.iid(58197).dodge=223;
-idb.iid(58197).barmor=2355;
-idb.iid(58197).rsock=1;
-idb.iid(58197).atype=1;
-idb.iid(58197).sb.sta=15;
-idb.iid(58197).sb.active=[1 0 0];
 
 idb.iid(50737).name='Havoc''s Call, Blade of Lordaeron Kings (Heroic)';
 idb.iid(50737).wtype='axe';
@@ -951,4 +671,324 @@ idb.iid(45463).sta=80;
 idb.iid(45463).agi=52;
 idb.iid(45463).hit=37;
 idb.iid(45463).exp=29;
+
+idb.iid(55059).name='Hardened Elementium Girdle';
+idb.iid(55059).ilvl=359;
+idb.iid(55059).str=253;
+idb.iid(55059).sta=380;
+idb.iid(55059).mast=169;
+idb.iid(55059).dodge=169;
+idb.iid(55059).barmor=1927;
+idb.iid(55059).atype=1;
+
+idb.iid(55058).name='Hardened Elementium Hauberk';
+idb.iid(55058).ilvl=359;
+idb.iid(55058).str=341;
+idb.iid(55058).sta=512;
+idb.iid(55058).exp=228;
+idb.iid(55058).parry=228;
+idb.iid(55058).barmor=3426;
+idb.iid(55058).atype=1;
+
+idb.iid(59359).name='Reinforced Bio-Optic Killshades';
+idb.iid(59359).ilvl=359;
+idb.iid(59359).str=301;
+idb.iid(59359).sta=512;
+idb.iid(59359).barmor=2784;
+idb.iid(59359).msock=1;
+idb.iid(59359).atype=1;
+idb.iid(59359).sb.str=20;
+idb.iid(59359).sb.active=[0 0 0];
+
+idb.iid(58483).name='Lifebound Alchemist Stone';
+idb.iid(58483).ilvl=359;
+idb.iid(58483).sta=380;
+idb.iid(58483).mast=213;
+idb.iid(58483).bsock=1;
+idb.iid(58483).sb.crit=10;
+idb.iid(58483).sb.active=[0 0 1];
+
+idb.iid(52352).name='Figurine - Earthen Guardian';
+idb.iid(52352).ilvl=346;
+idb.iid(52352).sta=197;
+
+idb.iid(56549).name='Twilight Dragonscale Cloak';
+idb.iid(56549).ilvl=359;
+idb.iid(56549).str=190;
+idb.iid(56549).sta=286;
+idb.iid(56549).mast=145;
+idb.iid(56549).dodge=96;
+idb.iid(56549).barmor=625;
+
+idb.iid(62351).name='Felsen''s Ring of Resolve';
+idb.iid(62351).ilvl=346;
+idb.iid(62351).str=168;
+idb.iid(62351).sta=252;
+idb.iid(62351).mast=112;
+idb.iid(62351).dodge=112;
+
+idb.iid(62432).name='Gryphon Rider''s Boots';
+idb.iid(62432).ilvl=359;
+idb.iid(62432).str=253;
+idb.iid(62432).sta=380;
+idb.iid(62432).mast=159;
+idb.iid(62432).parry=139;
+idb.iid(62432).barmor=2355;
+idb.iid(62432).ysock=1;
+idb.iid(62432).atype=1;
+idb.iid(62432).sb.parry=10;
+idb.iid(62432).sb.active=[0 1 0];
+
+idb.iid(62428).name='Crown of Wings';
+idb.iid(62428).ilvl=346;
+idb.iid(62428).str=196;
+idb.iid(62428).sta=454;
+idb.iid(62428).exp=137;
+idb.iid(62428).dodge=242;
+idb.iid(62428).barmor=2680;
+idb.iid(62428).msock=1;
+idb.iid(62428).rsock=1;
+idb.iid(62428).atype=1;
+idb.iid(62428).sb.dodge=30;
+idb.iid(62428).sb.active=[1 0 0];
+
+idb.iid(62449).name='Sandguard Bracers';
+idb.iid(62449).ilvl=359;
+idb.iid(62449).str=136;
+idb.iid(62449).sta=286;
+idb.iid(62449).exp=111;
+idb.iid(62449).dodge=190;
+idb.iid(62449).barmor=1499;
+idb.iid(62449).atype=1;
+
+idb.iid(62471).name='Mirror of Broken Images';
+idb.iid(62471).ilvl=359;
+idb.iid(62471).mast=321;
+
+idb.iid(57932).name='The Lustrous Eye';
+idb.iid(57932).ilvl=346;
+idb.iid(57932).str=168;
+idb.iid(57932).sta=252;
+idb.iid(57932).mast=112;
+idb.iid(57932).dodge=112;
+
+idb.iid(57914).name='Girdle of the Mountains';
+idb.iid(57914).ilvl=346;
+idb.iid(57914).str=225;
+idb.iid(57914).sta=337;
+idb.iid(57914).dodge=110;
+idb.iid(57914).parry=150;
+idb.iid(57914).barmor=1855;
+idb.iid(57914).rsock=1;
+idb.iid(57914).atype=1;
+idb.iid(57914).sb.sta=15;
+idb.iid(57914).sb.active=[1 0 0];
+
+idb.iid(58104).name='Sunburnt Pauldrons';
+idb.iid(58104).ilvl=346;
+idb.iid(58104).str=161;
+idb.iid(58104).sta=337;
+idb.iid(58104).hit=111;
+idb.iid(58104).dodge=205;
+idb.iid(58104).barmor=2474;
+idb.iid(58104).rsock=1;
+idb.iid(58104).atype=1;
+idb.iid(58104).sb.dodge=10;
+idb.iid(58104).sb.active=[1 0 0];
+
+idb.iid(58102).name='Greaves of Splendor';
+idb.iid(58102).ilvl=346;
+idb.iid(58102).str=210;
+idb.iid(58102).sta=454;
+idb.iid(58102).hit=133;
+idb.iid(58102).dodge=262;
+idb.iid(58102).barmor=2886;
+idb.iid(58102).bsock=1;
+idb.iid(58102).ysock=1;
+idb.iid(58102).atype=1;
+idb.iid(58102).sb.sta=30;
+idb.iid(58102).sb.active=[0 1 1];
+
+idb.iid(58103).name='Helm of the Proud';
+idb.iid(58103).ilvl=346;
+idb.iid(58103).str=302;
+idb.iid(58103).sta=454;
+idb.iid(58103).dodge=142;
+idb.iid(58103).parry=142;
+idb.iid(58103).barmor=2680;
+idb.iid(58103).msock=1;
+idb.iid(58103).atype=1;
+idb.iid(58103).sb.sta=45;
+idb.iid(58103).sb.active=[0 0 0];
+
+idb.iid(58105).name='Numbing Handguards';
+idb.iid(58105).ilvl=346;
+idb.iid(58105).str=215;
+idb.iid(58105).sta=337;
+idb.iid(58105).mast=140;
+idb.iid(58105).dodge=130;
+idb.iid(58105).barmor=2061;
+idb.iid(58105).bsock=1;
+idb.iid(58105).atype=1;
+idb.iid(58105).sb.dodge=10;
+idb.iid(58105).sb.active=[0 0 1];
+
+idb.iid(58101).name='Chestplate of the Steadfast';
+idb.iid(58101).ilvl=346;
+idb.iid(58101).str=282;
+idb.iid(58101).sta=454;
+idb.iid(58101).mast=182;
+idb.iid(58101).dodge=162;
+idb.iid(58101).barmor=3298;
+idb.iid(58101).bsock=1;
+idb.iid(58101).ysock=1;
+idb.iid(58101).atype=1;
+idb.iid(58101).sb.dodge=20;
+idb.iid(58101).sb.active=[0 1 1];
+
+idb.iid(57926).name='Shield of the Four Grey Towers';
+idb.iid(57926).ilvl=346;
+idb.iid(57926).str=168;
+idb.iid(57926).sta=252;
+idb.iid(57926).mast=112;
+idb.iid(57926).dodge=112;
+idb.iid(57926).barmor=11696;
+
+idb.iid(60354).name='Reinforced Sapphirium Chestguard';
+idb.iid(60354).ilvl=359;
+idb.iid(60354).str=341;
+idb.iid(60354).sta=512;
+idb.iid(60354).mast=208;
+idb.iid(60354).parry=168;
+idb.iid(60354).barmor=3426;
+idb.iid(60354).rsock=1;
+idb.iid(60354).atype=1;
+idb.iid(60354).sb.sta=30;
+idb.iid(60354).sb.active=[1 0 0];
+
+idb.iid(60355).name='Reinforced Sapphirium Handguards';
+idb.iid(60355).ilvl=359;
+idb.iid(60355).str=193;
+idb.iid(60355).sta=380;
+idb.iid(60355).mast=149;
+idb.iid(60355).parry=149;
+idb.iid(60355).barmor=2141;
+idb.iid(60355).bsock=1;
+idb.iid(60355).atype=1;
+idb.iid(60355).sb.mast=10;
+idb.iid(60355).sb.active=[0 0 1];
+
+idb.iid(60356).name='Reinforced Sapphirium Faceguard';
+idb.iid(60356).ilvl=359;
+idb.iid(60356).str=259;
+idb.iid(60356).sta=512;
+idb.iid(60356).hit=113;
+idb.iid(60356).dodge=281;
+idb.iid(60356).barmor=2784;
+idb.iid(60356).msock=1;
+idb.iid(60356).atype=1;
+idb.iid(60356).sb.sta=45;
+idb.iid(60356).sb.active=[0 0 0];
+
+idb.iid(60357).name='Reinforced Sapphirium Legguards';
+idb.iid(60357).ilvl=359;
+idb.iid(60357).str=341;
+idb.iid(60357).sta=512;
+idb.iid(60357).dodge=188;
+idb.iid(60357).parry=188;
+idb.iid(60357).barmor=2998;
+idb.iid(60357).rsock=1;
+idb.iid(60357).atype=1;
+idb.iid(60357).sb.sta=30;
+idb.iid(60357).sb.active=[1 0 0];
+
+idb.iid(60358).name='Reinforced Sapphirium Shoulderguards';
+idb.iid(60358).ilvl=359;
+idb.iid(60358).str=193;
+idb.iid(60358).sta=380;
+idb.iid(60358).hit=128;
+idb.iid(60358).dodge=213;
+idb.iid(60358).barmor=2570;
+idb.iid(60358).rsock=1;
+idb.iid(60358).atype=1;
+idb.iid(60358).sb.sta=15;
+idb.iid(60358).sb.active=[1 0 0];
+
+idb.iid(64676).name='Relic of Khaz''goroth';
+idb.iid(64676).ilvl=359;
+idb.iid(64676).str=107;
+idb.iid(64676).sta=161;
+idb.iid(64676).mast=72;
+idb.iid(64676).dodge=72;
+idb.iid(64676).rsock=1;
+
+idb.iid(58182).name='Bedrock Talisman';
+idb.iid(58182).ilvl=359;
+idb.iid(58182).sta=482;
+
+idb.iid(58187).name='Ring of the Battle Anthem';
+idb.iid(58187).ilvl=359;
+idb.iid(58187).str=190;
+idb.iid(58187).sta=286;
+idb.iid(58187).mast=127;
+idb.iid(58187).dodge=127;
+
+idb.iid(58192).name='Gray Hair Cloak';
+idb.iid(58192).ilvl=359;
+idb.iid(58192).str=136;
+idb.iid(58192).sta=286;
+idb.iid(58192).exp=111;
+idb.iid(58192).dodge=190;
+idb.iid(58192).barmor=625;
+
+idb.iid(58197).name='Rock Furrow Boots';
+idb.iid(58197).ilvl=359;
+idb.iid(58197).str=181;
+idb.iid(58197).sta=380;
+idb.iid(58197).exp=138;
+idb.iid(58197).dodge=223;
+idb.iid(58197).barmor=2355;
+idb.iid(58197).rsock=1;
+idb.iid(58197).atype=1;
+idb.iid(58197).sb.sta=15;
+idb.iid(58197).sb.active=[1 0 0];
+
+idb.iid(59521).name='Soul Blade';
+idb.iid(59521).wtype='swo';
+idb.iid(59521).ilvl=359;
+idb.iid(59521).tooldps=462.3;
+idb.iid(59521).swing=2.6;
+idb.iid(59521).avgdmg=1202;
+idb.iid(59521).str=146;
+idb.iid(59521).sta=219;
+idb.iid(59521).hit=97;
+idb.iid(59521).mast=97;
+
+idb.iid(59221).name='Massacre Treads';
+idb.iid(59221).ilvl=359;
+idb.iid(59221).str=233;
+idb.iid(59221).sta=380;
+idb.iid(59221).exp=149;
+idb.iid(59221).mast=169;
+idb.iid(59221).barmor=2355;
+idb.iid(59221).bsock=1;
+idb.iid(59221).atype=1;
+idb.iid(59221).sb.str=10;
+idb.iid(59221).sb.active=[0 0 1];
+
+idb.iid(59470).name='Bracers of Impossible Strength';
+idb.iid(59470).ilvl=359;
+idb.iid(59470).str=190;
+idb.iid(59470).sta=286;
+idb.iid(59470).mast=127;
+idb.iid(59470).parry=127;
+idb.iid(59470).barmor=1499;
+idb.iid(59470).atype=1;
+
+idb.iid(59233).name='Bile-O-Tron Nut';
+idb.iid(59233).ilvl=359;
+idb.iid(59233).str=136;
+idb.iid(59233).sta=286;
+idb.iid(59233).exp=111;
+idb.iid(59233).dodge=190;
 
