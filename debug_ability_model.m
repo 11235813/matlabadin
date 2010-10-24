215e3809414b50fdffa24c8a50b7b006f3748ec4
theckhd <theckhd@gmail.com>
2010-10-24 02:48:25 +0000

    [AM] update, JoTruth coefficients still questionable; [SM] reverted TbtL to 6%, debug module updated with data

--- debug_ability_model.m
+++ debug_ability_model.m
@@ -1,29 +1,41 @@
 clear;
-gear_db;
-%invoke player model. race : 1=human, 2=dwarf, 3=draenei, 4=belf, 5=tauren.
-base=player_model('race',1);
-%invoke npc model
-npc=npc_model(base,'lvl',82);
+gear_db;def_db;
+
+%player and dummy
+base=player_model('lvl',80,'race','Human','prof','');
+npc=npc_model(base,'lvl',83);
+
+
 %execution. seal : 1=truth, 2=righteousness, 3=insight, 4=justice.
-exec=execution_model('npccount',1,'timein',1,'timeout',1,'seal',1);
-%invoke talents & glyphs
-talents
-talent.RuleofLaw=0;
-%load gear set
-gear_sample;
-%calculate relevant stats
-gear_stats
-%activate buffs
+exec=execution_model('npccount',3,'timein',0,'timeout',1,'seal',1);
 buff=buff_model('mode',1);
+
+%invoke talents & glyphs
+talent=ddb.talentset{1}; 
+talent.ret(1,2)=3; %3/3 crusade
+talent.prot(3,1)=0; %Hallowed Ground
+glyph.prime=[0 0 0 1 1 1 0];
+glyph.major=[0 0 0 0 0 0 0 0 0 0];
+
+talents
+
+%gear set
+egs=ddb.gearset{2}; %naked with Dalaran Sword
+gear_stats
+
 %calculate final stats
 stat_model
 
 
 %% Insert appropriate character info
-player.ap=3562;
-player.hsp=992;
-gear.swing=1.7;
-player.wdamage=(677+877)/2;
+player.ap=3980;
+player.hsp=1216;
+gear.swing=1.6;
+gear.avgdmg=(296+551)/2;
+
+%recalculate wdmg and ndmg
+player.wdamage=gear.avgdmg+player.ap./14.*gear.swing;
+player.ndamage=gear.avgdmg+player.ap./14.*2.4;
 
 
 %calculate ability output
@@ -32,8 +44,6 @@ ability_model
 %generate a damage summary array
 %% Summary
 dmg_labels={'ShoR';'CS';'JoT';'AS';'HW';'HoW';'Exor';'SoT';'SoR';'SoJ';'Cens';'Cons';'HotR';'HaNova';'Melee'};
-
-
 raw_vals=[round(raw.ShieldoftheRighteous); 
     round(raw.CrusaderStrike); 
     round(raw.Judgement); 
@@ -49,25 +59,14 @@ raw_vals=[round(raw.ShieldoftheRighteous);
     round(raw.HammeroftheRighteous);
     round(raw.HammerNova);
     round(raw.Melee)];
-
-dmg_vals=[round(dmg.ShieldoftheRighteous); 
-    round(dmg.CrusaderStrike); 
-    round(dmg.Judgement); 
-    round(dmg.AvengersShield); 
-    round(dmg.HolyWrath); 
-    round(dmg.HammerofWrath); 
-    round(dmg.Exorcism); 
-    round(dmg.SealofTruth); 
-    round(dmg.SealofRighteousness); 
-    round(dmg.SealofJustice); 
-    round(dmg.Censure); 
-    round(dmg.Consecration); 
-    round(dmg.HammeroftheRighteous);
-    round(dmg.HammerNova);
-    round(dmg.Melee)];
-
 spacer=repmat(' ',size(raw_vals,1),2);
 raw_summary=[char(dmg_labels) spacer int2str(raw_vals)];
-dmg_summary=[char(dmg_labels) spacer int2str(dmg_vals)];
-thr_summary=[char(dmg_labels) spacer int2str(dmg_vals.*mdf.threat)];
-all_summary=[char(dmg_labels) spacer int2str(raw_vals) spacer int2str(dmg_vals) spacer int2str(dmg_vals.*mdf.threat)]
+
+%Kierly, 522 AP / 178 SP
+data1=[713.5 257.4 372.5 3948.6 812.9 0 2209.6 27.2 16 7 119.2*5 92.6*10 28.2 977.4 66.5]';
+%Kierly  522 AP / 978 SP
+data1=[695.7 250.7 901.7 4134.8 1226.4 0 2322.2 26.4 45.3 19.4 189.5*5 113.5*10 27.8 974.8 66.5]';
+%Rabs  3980 AP / 1216 Sp
+data1=[5260.2 1852 2736.5 4692.4 1350.3 0 2984.8 207.2 134.2 0 851.2*5 213.2*10 203.6 1782.3 520.1]';
+%[ShoR CS Jud AS HW HoW Exor SoT SoR SoJ Cens Cons HotR HaNova melee]
+data2=[raw_summary spacer int2str(data1) spacer int2str(data1-raw_vals) spacer int2str(100.*(data1-raw_vals)./raw_vals)]
