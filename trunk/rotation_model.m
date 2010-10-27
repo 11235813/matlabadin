d9b78e8dc9d457899007c1004ca2dbd7f51dbf10
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-10-27 02:02:01 +0000

    [CT] updates

--- trunk/rotation_model.m
+++ trunk/rotation_model.m
@@ -13,11 +13,6 @@
 %2 ShoR
 %1 "X" that could be AS, HW, or potentially Cons
 
-%set to 1 if you want to cast Cons in slot X (max once every 4th X,
-%regardless of glyph)
-if exist('rot')==0 || isfield(rot,'consflag')==0
-    rot.consflag=0;
-end
 
 [temphit tempn]=meshgrid(mdf.mehit,[1:15]);
 rot.xtragcd=mdf.mehit.*sum(tempn.*(1-temphit).^(tempn-1))-1;
@@ -29,12 +24,28 @@ P=mean(cols(length(cols)-1:length(cols),1));
 
 rot.val.ones=ones(size(mdf.mehit));
 rot.labels={'ShoR';'CS';'J';'AS';'HW';'Cons';'HotR';'2ShoR';'Inq';'Seal';'HaNova';'HoW'};
+%% HW>Cons
 rot.numcasts=[2.*(mdf.phcrit+mdf.mehit.*mdf.SacDut.*(mdf.phcritmulti-mdf.phcrit));... %ShoR
     6.*rot.val.ones;...                                            %CS
     2.*rot.val.ones;...                                            %J
     2*P.*rot.val.ones;...                                          %AS
-    max([2*(1-P)-0.5.*rot.consflag; 0]).*rot.val.ones;...          %HW
-    0.5.*rot.consflag.*rot.val.ones;...                            %Cons
+    max([2*(1-P); 0]).*rot.val.ones;...          %HW
+    0.*rot.val.ones;...                            %Cons
+    0.*rot.val.ones;...                                            %HotR
+    0.*rot.val.ones;...                                            %2ShoR
+    0.*rot.val.ones;...                                            %Inq
+    0.*rot.val.ones;...                                            %HoW
+    8.*mdf.mehit+2.*mdf.rahit.*(mdf.JotJ>0).*rot.val.ones;...      %seal (CS+ShoR+J)
+    0.*rot.val.ones];                                             %hammer nova
+
+%% Cons>HW
+rot1=rot;
+rot1.numcasts=[2.*(mdf.phcrit+mdf.mehit.*mdf.SacDut.*(mdf.phcritmulti-mdf.phcrit));... %ShoR
+    6.*rot.val.ones;...                                            %CS
+    2.*rot.val.ones;...                                            %J
+    2*P.*rot.val.ones;...                                          %AS
+    max([2*(1-P)-0.5; 0]).*rot.val.ones;...          %HW
+    0.5.*rot.val.ones;...                            %Cons
     0.*rot.val.ones;...                                            %HotR
     0.*rot.val.ones;...                                            %2ShoR
     0.*rot.val.ones;...                                            %Inq
@@ -43,25 +54,33 @@ rot.numcasts=[2.*(mdf.phcrit+mdf.mehit.*mdf.SacDut.*(mdf.phcritmulti-mdf.phcrit)
     0.*rot.val.ones];                                             %hammer nova
 
 rot.coeff=rot.numcasts./repmat((18+1.5.*2.*rot.xtragcd),size(rot.numcasts,1),1);
+rot1.coeff=rot1.numcasts./repmat((18+1.5.*2.*rot.xtragcd),size(rot1.numcasts,1),1);
+
 rot.acdps=sum(rot.coeff'*pridmg);
+rot1.acdps=sum(rot1.coeff'*pridmg);
 
 rot.padps=0;
+rot1.padps=0;
 if strcmpi('Truth',exec.seal)||strcmpi('SoT',exec.seal)
     rot.padps=rot.padps+dps.Censure;
+    rot1.padps=rot1.padps+dps.Censure;
 end
 
 %aa and seal damage
 rot.padps=rot.padps+dps.Melee+dmg.activeseal.*mdf.mehit./player.wswing;
+rot1.padps=rot1.padps+dps.Melee+dmg.activeseal.*mdf.mehit./player.wswing;
 
 rot.totdps=rot.acdps+rot.padps;
+rot1.totdps=rot1.acdps+rot1.padps;
 
 %Inq handling
 rot.Inq=0;
+rot1.Inq=0;
 
-%coeffs for ability damage sim
-if length(rot.val.ones)==1
-    rot.admgcoeffs=(rot.numcasts+[0 0 0 0 0 0.5 6 0 0 0 6 0]')./(18+1.5.*2.*rot.xtragcd);
-end
+%coeffs for ability damage sim - will implement later
+% if length(rot.val.ones)==1
+%     rot.admgcoeffs=(rot.numcasts+[0 0 0 0 0 0.5 6 0 0 0 6 0]')./(18+1.5.*2.*rot.xtragcd);
+% end
 
 %% Alternative rotation with HotR instead of CS
 
@@ -69,8 +88,8 @@ rot2.numcasts=[2.*(mdf.phcrit+mdf.mehit.*mdf.SacDut.*(mdf.phcritmulti-mdf.phcrit
     0.*rot.val.ones;...                                            %CS
     2.*rot.val.ones;...                                            %J
     2*P.*rot.val.ones;...                                          %AS
-    max([2*(1-P)-0.5.*rot.consflag; 0]).*rot.val.ones;...          %HW
-    0.5.*rot.consflag.*rot.val.ones;...                            %Cons
+    max([2*(1-P)-0.5; 0]).*rot.val.ones;...          %HW
+    0.5.*rot.val.ones;...                            %Cons
     6.*rot.val.ones;...                                            %HotR
     0.*rot.val.ones;...                                            %2ShoR
     0.*rot.val.ones;...                                            %Inq
