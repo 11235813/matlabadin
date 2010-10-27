c2c5cdad1750d1efda3f9c73084fdc6c859b5506
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-10-27 02:22:45 +0000

    [CG] update

--- calc_glyphs.m
+++ calc_glyphs.m
@@ -81,41 +81,11 @@ gear.exp=99;consum.exp=0;
 %calculate final stats
 stat_model
 ability_model
-prio_model
-rot.consflag=1;
 rotation_model
 
-%run sim
-% seq1=prio_sim(10,'N',90000);
-%or not
-seq1=rot;
-seq2=aoe;
-
-
-%active DPS
-acdps(1)=sum(pridmg.*seq1.coeff);
-acdps2(1)=sum(pridmg.*seq2.coeff);
-      
-%calculate passive damage sources
-padps(1)=0;
-padps2(1)=0;
-%account for Inq
-Inqmod=sum(seq1.Inq>0)./length(seq1.Inq);
-Inqmod2=sum(seq2.Inq>0)./length(seq2.Inq);
-
-%assume a 5-stack of SoT (if applicable).
-if strcmpi('Truth',exec.seal)||strcmpi('SoT',exec.seal)
-    padps(1)=padps(1)+dps.Censure.*(1+0.3.*Inqmod);
-    padps2(1)=padps2(1)+dps.Censure.*(1+0.3.*Inqmod2);
-end
-
-%aa and seal damage
-padps(1)=padps(1)+dps.Melee+dmg.activeseal.*mdf.mehit.*(1+0.3.*Inqmod)./player.wswing;
-padps2(1)=padps2(1)+dps.Melee+dmg.activeseal.*mdf.mehit.*(1+0.3.*Inqmod2)./player.wswing;
-
-%now calculate total DPS
-totdps(1)=acdps(1)+padps(1);
-totdps2(1)=acdps2(1)+padps2(1);
+totdps(1)=rot.totdps;
+totdps1(1)=rot1.totdps;
+totdps2(1)=rot2.totdps;
 tabledps(1,:)=[totdps totdps];
 
 % for m=2:length(tree)-2  %everything except GC & SD
@@ -133,35 +103,15 @@ for m=2:length(gtree) %everything
     stat_model
     ability_model
     rotation_model;
-    seq1=rot;
-    seq2=aoe;
     
-    
-    %active dps
-    acdps(m)=sum(pridmg.*seq1.coeff);
-    acdps2(m)=sum(pridmg.*seq2.coeff);
-    padps(m)=0;
-    padps2(m)=0;
-    %account for Inq
-    Inqmod=sum(seq1.Inq>0)./length(seq1.Inq);
-    Inqmod2=sum(seq2.Inq>0)./length(seq2.Inq);
-
-    %assume a 5-stack of SoT (if applicable).
-    if strcmpi('Truth',exec.seal)||strcmpi('SoT',exec.seal)
-        padps(m)=padps(m)+dps.Censure.*(1+0.3.*Inqmod);
-        padps2(m)=padps2(m)+dps.Censure.*(1+0.3.*Inqmod2);
-
-    end
-
-    %aa and seal damage
-    padps(m)=padps(m)+dps.Melee+dmg.activeseal.*mdf.mehit.*(1+0.3.*Inqmod)./player.wswing;
-    padps2(m)=padps2(m)+dps.Melee+dmg.activeseal.*mdf.mehit.*(1+0.3.*Inqmod2)./player.wswing;
-
-    totdps(m)=acdps(m)+padps(m);
-    totdps2(m)=acdps2(m)+padps2(m);
+    totdps(m)=rot.totdps;
+    totdps1(m)=rot1.totdps;
+    totdps2(m)=rot2.totdps;
     
     if strcmp(name(m),'HotR')
         tabledps(m,:)=[totdps2(1) totdps2(m)];
+    elseif strcmp(name(m),'Cons')
+        tabledps(m,:)=[totdps1(1) totdps1(m)];
     else
         tabledps(m,:)=[totdps(1) totdps(m)];
     end
