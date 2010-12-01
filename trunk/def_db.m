dd9671896527b498a4231e0989133c05457dce06
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-12-01 03:18:11 +0000

    default gear sets updated

--- trunk/def_db.m
+++ trunk/def_db.m
@@ -3,91 +3,64 @@
 
 %% Gear Sets
 
-%%Naked gear set with premade mace/shield
-tempegs(1)=equip(1);  %head
-tempegs(2)=equip(1); %neck
-tempegs(3)=equip(1);%shoulders
-tempegs(4)=equip(1);%back
-tempegs(5)=equip(1);%chest
-tempegs(6)=equip(1);%wrists
-tempegs(7)=equip(1);%hands
-tempegs(8)=equip(1);%waist
-tempegs(9)=equip(1);%legs
-tempegs(10)=equip(1);%feet
-tempegs(11)=equip(1);%finger0
-tempegs(12)=equip(1);%finger1
-tempegs(13)=equip(1);%trinket0
-tempegs(14)=equip(1);%trinket1
-tempegs(15)=equip(63799);%main hand
-tempegs(16)=equip(56096);%off hand
-tempegs(17)=equip(1);%ranged
+%%Starter pre-raid
+tempegs(1)=equip(58103);  %Helm of the Proud
+tempegs(2)=equip(57932); %Lustrous Eye
+tempegs(3)=equip(58103);%Sunburnt Pauldrons
+tempegs(4)=equip(56549); %Twilight Dragonscale
+tempegs(5)=equip(58101); %Chestplate of the Steadfast
+tempegs(6)=equip(57870); %Alpha Bracers
+tempegs(7)=equip(58105); %Numbing Handguards
+tempegs(8)=equip(55059); %Hardened Elementium
+tempegs(9)=equip(58102); %Greaves of Splendor
+tempegs(10)=equip(62432); %Gryphon Rider's Boots
+tempegs(11)=equip(62440); %Red Rock Band
+tempegs(12)=equip(62351); %Felsen's Ring
+tempegs(13)=equip(56347); %Leaden Despair
+tempegs(14)=equip(56406); %Impetuous Query
+tempegs(15)=equip(56346); %Elementium Fang
+tempegs(16)=equip(57926); %Shield of hte Four Grey Towers
+tempegs(17)=equip(64676); %Relic of Khaz
 %%Enchants (21-37)
-tempegs(21)=equip(1,'s');%head
+tempegs(21)=equip(86931,'s');%Earthen Ring
 % tempegs(22)=equip(2);%neck
-tempegs(23)=equip(1,'s');%shoulders
-tempegs(24)=equip(1,'s');%back
-tempegs(25)=equip(1,'s');%chest
-tempegs(26)=equip(1,'s');%wrists
-tempegs(27)=equip(1,'s');%hands
+tempegs(23)=equip(86847,'s');%Unbreakable Quartz
+tempegs(24)=equip(74234,'s');%Protection
+tempegs(25)=equip(74251,'s');%Greater STamina
+tempegs(26)=equip(74229,'s');%Dodge
+tempegs(27)=equip(74255,'s');%Greater Mastery
 % tempegs(28)=equip(4);%waist
-tempegs(29)=equip(1,'s');%legs
-tempegs(30)=equip(1,'s');%feet
-tempegs(31)=equip(1,'s');%finger0
-tempegs(32)=equip(1,'s');%finger1
+tempegs(29)=equip(78172,'s');%Charscale
+tempegs(30)=equip(74238,'s');%Mastery
+tempegs(31)=equip(74218,'s');%finger0
+tempegs(32)=equip(74218,'s');%finger1
 % tempegs(33)=equip(2);%trinket0
 % tempegs(34)=equip(2);%trinket1
-tempegs(35)=equip(1,'s');%main hand
-tempegs(36)=equip(1,'s');%off hand
+tempegs(35)=equip(74244,'s');%Windwalk?
+tempegs(36)=equip(34009,'s');%Stamina, for now
 % tempegs(37)=equip(4);%ranged
 ddb.gearset{1}=tempegs;
 
-%Naked with Dalaran Sword
-tempegs(15)=equip(44638);
-tempegs(16)=equip(1);
+%Progression Raid set
+tempegs(1)=equip(60356);  %T11
+tempegs(2)=equip(59319); %Ironstar
+tempegs(3)=equip(60358); %T11
+tempegs(4)=equip(62383); %Great Turtle
+tempegs(5)=equip(60354); %T11
+tempegs(6)=equip(59470); %Impossible Strength
+tempegs(7)=equip(60355); %T11
+tempegs(8)=equip(55059); %Hardened Elementium
+tempegs(9)=equip(60357); %T11
+tempegs(10)=equip(62432); %Gryphon Rider's Boots
+tempegs(11)=equip(58187); %Ring of the Battle Anthem
+tempegs(12)=equip(59233); %Bile-o-tron Nut
+tempegs(13)=equip(59515); %Vial of Stolen Memories
+tempegs(14)=equip(62468); %Unsolvable Riddle
+tempegs(15)=equip(59347); %Mace of Acrid Death
+tempegs(16)=equip(67145); %Blockade's Lost Shield
+tempegs(17)=equip(64676); %Relic of Khaz
 
 ddb.gearset{2}=tempegs;
-
-%Naked
-tempegs(15)=equip(1);
-ddb.gearset{3}=tempegs;
-
-%premade gear set
-tempegs(1)=equip(00101);  %head
-tempegs(2)=equip(55888); %neck
-tempegs(3)=equip(56124);%shoulders
-tempegs(4)=equip(56219);%back
-tempegs(5)=equip(66933);%chest
-tempegs(6)=equip(55992);%wrists
-tempegs(7)=equip(56099);%hands
-tempegs(8)=equip(00103);%waist
-tempegs(9)=equip(10103);%legs
-tempegs(10)=equip(10104);%feet
-tempegs(11)=equip(56111);%finger0
-tempegs(12)=equip(10105);%finger1
-tempegs(13)=equip(56121);%trinket0
-tempegs(14)=equip(10106);%trinket1
-tempegs(15)=equip(63799);%main hand
-tempegs(16)=equip(56096);%off hand
-tempegs(17)=equip(63888);%ranged
-%%Enchants (21-37)
-tempegs(21)=equip(86931,'s');%head
-% tempegs(22)=equip(2);%neck
-tempegs(23)=equip(86847,'s');%shoulders
-tempegs(24)=equip(74234,'s');%back
-tempegs(25)=equip(74251,'s');%chest
-tempegs(26)=equip(62256,'s');%wrists
-tempegs(27)=equip(74255,'s');%hands
-% tempegs(28)=equip(4);%waist
-tempegs(29)=equip(78170,'s');%legs
-tempegs(30)=equip(74189,'s');%feet
-tempegs(31)=equip(1,'s');%finger0
-tempegs(32)=equip(1,'s');%finger1
-% tempegs(33)=equip(2);%trinket0
-% tempegs(34)=equip(2);%trinket1
-tempegs(35)=equip(59619,'s');%main hand
-tempegs(36)=equip(74207,'s');%off hand
-% tempegs(37)=equip(4);%ranged
-ddb.gearset{4}=tempegs;
 
 
 
