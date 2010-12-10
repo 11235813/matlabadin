7be8f51accf2d0e507c9d8c6b6ed1f100237009b
theckhd@gmail.com <theckhd@gmail.com@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2010-12-10 16:18:09 +0000

    [CW] minor tweaks

--- trunk/calc_weapons.m
+++ trunk/calc_weapons.m
@@ -27,29 +27,29 @@ egs(1).exp=max([egs(1).exp 0])-(player.exp-26).*cnv.exp_exp;
 egs(1).mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
 helm2=egs(1);
 
-weaplist1=[56346; %Elementium Fang (Heroic)
+weaplist1=[62457; %Ravening Slicer
+           65173; %Thief''s Blade (Heroic)
+           56346; %Elementium Fang (Heroic)
+           67602; %Elementium Gutslicer
+           56459; %Mace of Transformed Bone (Heroic)
            56364; %Axe of the Eclipse (Heroic)
            56430; %Sun Strike (Heroic)
-           56459; %Mace of Transformed Bone (Heroic)
+           65171; %Cookie''s Tenderizer (Heroic)
+           63533; %Fang of Twilight
            59347; %Mace of Acrid Death
            59521; %Soul Blade
-           62457; %Ravening Slicer
-           63533; %Fang of Twilight
-           65036; %Mace of Acrid Death (Heroic)
            65094; %Fang of Twilight (Heroic)
+           65036; %Mace of Acrid Death (Heroic)
            65103; %Soul Blade (Heroic)
-           65171; %Cookie''s Tenderizer (Heroic)
-           65173; %Thief''s Blade (Heroic)
-           67602; %Elementium Gutslicer
           ];
 
-weaplist2=[55065; %Elementium Hammer
+weaplist2=[56433; %Blade of the Burning Sun (Heroic)
+           55065; %Elementium Hammer
            56312; %Torturer''s Mercy (Heroic)
-           56433; %Blade of the Burning Sun (Heroic)
            57872; %Scepter of Power (Heroic)
-           59459; %Andoros, Fist of the Dragon King
-           59463; %Maldo''s Sword Cane
            62459; %Shimmering Morningstar
+           59463; %Maldo''s Sword Cane
+           59459; %Andoros, Fist of the Dragon King
            63680; %Twilight''s Hammer
            65013; %Maldo''s Sword Cane (Heroic)
            65017; %Andoros, Fist of the Dragon King (Heroic)
@@ -57,21 +57,21 @@ weaplist2=[55065; %Elementium Hammer
           ];
       
           
-weaplist3=[55067; %Elementium Bonesplitter
+weaplist3=[65166; %Buzz Saw (Heroic)
            56266; %Lightning Whelk Axe (Heroic)
-           56353; %Heavy Geode Mace (Heroic)
-           56384; %Resonant Kris (Heroic)
            56396; %Hammer of Sparks (Heroic)
-           59333; %Lava Spine
+           56384; %Resonant Kris (Heroic)
+           65164; %Cruel Barb (Heroic)
+           56353; %Heavy Geode Mace (Heroic)
+           55067; %Elementium Bonesplitter
+           67605; %Forged Elementium Mindcrusher
+           65170; %Smite''s Reaver (Heroic)
            59443; %Crul''korak, the Lightning''s Arc
+           68161; %Krol Decapitator
+           59333; %Lava Spine
            64885; %Scimitar of the Sirocco
            65024; %Crul''korak, the Lightning''s Arc (Heroic)
            65047; %Lava Spine (Heroic)
-           65164; %Cruel Barb (Heroic)
-           65166; %Buzz Saw (Heroic)
-           65170; %Smite''s Reaver (Heroic)
-           67605; %Forged Elementium Mindcrusher
-           68161; %Krol Decapitator
           ];
 weaplist=[weaplist1;weaplist2;weaplist3];
           
@@ -125,26 +125,26 @@ dps=dps1;
 y1=1:M1;y2=M1+1+[1:M2];y3=max(y2)+1+[1:M3];
 y=[y1 y2 y3];
 
-dps1p=[dps1(:,1) diff(dps1')'];
+dps1p=[dps1(:,1) diff(dps1')']./1000;
 
 figure(70)
-set(gcf,'Position',[2200 12 724 936])
+% set(gcf,'Position',[2200 12 724 936])
 bar70=barh(y,dps1p,'BarWidth',0.5,'BarLayout','stacked');
 set(bar70(2),'FaceColor',[0.749 0.749 0]);
-xlim([0.99.*min(min(dps1)) 1.01.*max(max(dps1))])
+xlim([0.99.*min(dps1p(:,1)) 1.01.*max(sum(dps1p,2))])
 ylim(0.5+[0 max(y)])
 set(gca,'YTick',y,'YTickLabel',pinfo.labels)
-xlabel('DPS')
+xlabel('DPS (thousands)')
 title('All by ilvl & category - CS rotation')
 legend('4% hit 18 exp','8% hit 26 exp','Location','Best')
 
 
-[x71 k71]=sort(dps(1:M1,1));y71=y1(k71);
+[x71 k71]=sort(dps1p(1:M1,1));y71=y1(k71);
 figure(71)
 % set(gcf,'Position',[2240 88 724 579])
 bar71=barh(dps1p(k71,:),'BarWidth',0.5,'BarLayout','stacked');
 set(bar71(2),'FaceColor',[0.749 0.749 0]);
-xlim([0.99.*min(dps1(k71,1)) 1.01.*max(dps1(k71,2))])
+xlim([0.99.*min(dps1p(k71,1)) 1.01.*max(sum(dps1p(k71,:),2))])
 ylim(0.5+[0 max(y1)])
 set(gca,'YTick',y1,'YTickLabel',pinfo.labels(k71,:))
 xlabel('DPS')
@@ -154,7 +154,7 @@ legend('4% hit 18 exp','8% hit 26 exp','Location','Best')
 dps2p=[dps2(:,1) diff(dps2')'];
 
 figure(72)
-set(gcf,'Position',[2200 12 724 936])
+% set(gcf,'Position',[2200 12 724 936])
 bar72=barh(y,dps2p,'BarWidth',0.5,'BarLayout','stacked');
 set(bar72(2),'FaceColor',[0.749 0.749 0]);
 xlim([0.99.*min(min(dps2)) 1.01.*max(max(dps2))])
