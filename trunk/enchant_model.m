99e0ecfaf4513a0ef4df28bf99be19b6f28e48e6
tlitp0 <tlitp0@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2011-01-10 18:39:43 +0000

    - updated models for VengAP and Av

--- trunk/enchant_model.m
+++ trunk/enchant_model.m
@@ -14,80 +14,87 @@ end
 
 
 %% Windwalk
+%formulation
 ww.pc=gear.swing./60;ww.pd=10;
-ww.q=(1-ww.pc.*mdf.mehit).^(ww.pd./player.wswing) ...            %AA
-    .*(1-ww.pc.*mdf.mehit).^(ww.pd.*seq.cps(2)) ...            %CS
-    .*(1-ww.pc).^(ww.pd.*seq.cps(1)) ...             %SotR
-    .*(1-ww.pc.*mdf.rahit).^(ww.pd.*seq.cps(3)) ...     %J
-    .*(1-ww.pc.*mdf.rahit).^(ww.pd.*seq.cps(4));               %AS
+%output
+dtrack=[];dstore=gear.dodge;
+for dloop=0:600:600
+    gear.dodge=gear.dodge+dloop;
+    stat_model;ability_model;rotation_model;
+    dtrack=[dtrack player.dodge];
+end
+%average effectiveness
+ww.q=(1-ww.pc.*mdf.mehit).^(ww.pd./player.wswing) ... %AA
+    .*(1-ww.pc.*mdf.mehit).^(ww.pd.*seq.cps(2)) ...   %CS
+    .*(1-ww.pc).^(ww.pd.*seq.cps(1)) ...              %SotR
+    .*(1-ww.pc.*mdf.rahit).^(ww.pd.*seq.cps(3)) ...   %J
+    .*(1-ww.pc.*mdf.rahit).^(ww.pd.*seq.cps(4));      %AS
 ww.p=1-ww.q;
-dtrack=[];
-for dloop=0:600:1800
-    tmpavd=avoid_dr(base,gear.dodge+consum.dodge+dloop,gear.parry+consum.parry ...
-        +floor((player.str-base.stats.str)./4),player.agi-base.stats.agi);
-    dtrack=[dtrack tmpavd.dodgedr];
-end
-ww.tm=[ww.q ww.p    0    0;
-    ww.q    0 ww.p    0;
-    ww.q    0    0 ww.p;
-    ww.q    0    0 ww.p];
-ww.dd=ww.tm^100;
-ww.upt=ww.dd(1,:);
-ww.gupt=1-ww.upt(1,1);
-ww.stacks=(ww.upt(2)+2.*ww.upt(3)+3.*ww.upt(4))./ww.gupt;
-ww.dodge=0;
-for dloop=1:4
-    ww.dodge=ww.dodge+ww.upt(dloop).*dtrack(dloop);
-end
-ww.dodge(1)=ww.dodge-dtrack(1);
-ww.dodge(2)=ww.p.*(dtrack(2)-dtrack(1));
-% [ww.gupt ww.stacks ww.dodge(1) ww.dodge(2)]
+ww.dodge=ww.p.*(dtrack(2)-dtrack(1));
+%cleanup
+gear.dodge=dstore;
+clear dtrack dstore
 
 
 %% Landslide
+%formulation
 ls.pc=gear.swing./60;ls.pd=12;
-ls.q=(1-ls.pc.*mdf.mehit).^(ls.pd./player.wswing) ...            %AA
-    .*(1-ls.pc.*mdf.mehit).^(ls.pd.*seq.cps(2)) ... %CS
-    .*(1-ls.pc).^(ls.pd.*seq.cps(1)) ...               %SotR
-    .*(1-ls.pc.*mdf.rahit).^(ls.pd.*seq.cps(3)) ...    %J
-    .*(1-ls.pc.*mdf.rahit).^(ls.pd.*seq.cps(4)); %AS
-ls.p=1-ls.q;
+%reference output
+stat_model;ability_model;rotation_model;
 ls.base=rot.totdps;
-gear.ap=gear.ap+1000;stat_model;ability_model;rotation_model;
+%proc-based output
+astore=gear.ap;gear.ap=gear.ap+1000;
+stat_model;ability_model;rotation_model;
+%average effectiveness
+ls.q=(1-ls.pc.*mdf.mehit).^(ls.pd./player.wswing) ... %AA
+    .*(1-ls.pc.*mdf.mehit).^(ls.pd.*seq.cps(2)) ...   %CS
+    .*(1-ls.pc).^(ls.pd.*seq.cps(1)) ...              %SotR
+    .*(1-ls.pc.*mdf.rahit).^(ls.pd.*seq.cps(3)) ...   %J
+    .*(1-ls.pc.*mdf.rahit).^(ls.pd.*seq.cps(4));      %AS
+ls.p=1-ls.q;
 ls.dps=(rot.totdps-ls.base).*ls.p;
-% [ls.p ls.dps]
+%cleanup
+gear.ap=astore;
+clear astore
 
 
 %% Avalanche
-av.ppc=5.*gear.swing./60;av.spc=0.1;
+%formulation
+av.ppc=5.*gear.swing./60;av.spc=0.2;av.sicd=10;
 av.dpp=500.*mdf.spdmg.*mdf.spcrit.*target.resrdx;
-av.ptps=mdf.mehit./player.wswing ...        %AA
-    +mdf.mehit.*seq.cps(2) ... %CS
-    +seq.cps(1) ...            %SotR
-    +mdf.rahit.*seq.cps(3) ...    %J
-    +mdf.rahit.*seq.cps(4); %AS
-av.stps=1./cens.NetTick ...                                           %Cens
-    +max([0.75-P.CS;rot.val.zeros]).*mdf.sphit./(9+1.5.*rot.xtragcd); %HW
-av.pps=av.ptps.*av.ppc+av.stps.*av.spc;
+%trigger rates
+stat_model;ability_model;rotation_model;
+av.ptps=mdf.mehit./player.wswing ... %AA
+    +mdf.mehit.*seq.cps(2) ...       %CS
+    +seq.cps(1) ...                  %SotR
+    +mdf.rahit.*seq.cps(3) ...       %J
+    +mdf.rahit.*seq.cps(4);          %AS
+av.stps=1./cens.NetTick ... %Cens
+    +mdf.sphit.*seq.cps(5); %HW
+%effective cooldown/proc chance for spell-based triggers
+av.effsicd=ceil(av.sicd.*av.stps)./av.stps;
+av.effspc=1./(av.stps.*(av.effsicd+1./(av.stps.*av.spc)));
+%output
+av.pps=av.ptps.*av.ppc+av.stps.*av.effspc;
 av.dps=av.dpp.*av.pps;
-% [av.dpp av.pps av.dps]
 
 
 %% Hurricane
+%formulation
 hu.ppc=gear.swing./60;hu.spc=0.15;hu.pd=12;hu.sicd=45;
-htrack=zeros(1,4);
-hstore=gear.haste;
+%output
+htrack=zeros(1,4);hstore=gear.haste;
 for i=1:5
     gear.haste=hstore+450.*sum(htrack(i,2:3));
     stat_model;ability_model;
-    hu.q(1)=(1-hu.ppc.*mdf.mehit).^(hu.pd./player.wswing) ...            %AA
-        .*(1-hu.ppc.*mdf.mehit).^(hu.pd.*seq.cps(2)) ... %CS
-        .*(1-hu.ppc).^(hu.pd.*seq.cps(1)) ...               %SotR
-        .*(1-hu.ppc.*mdf.rahit).^(hu.pd.*seq.cps(3)) ...    %J
-        .*(1-hu.ppc.*mdf.rahit).^(hu.pd.*seq.cps(4)); %AS
+    hu.q(1)=(1-hu.ppc.*mdf.mehit).^(hu.pd./player.wswing) ... %AA
+        .*(1-hu.ppc.*mdf.mehit).^(hu.pd.*seq.cps(2)) ...      %CS
+        .*(1-hu.ppc).^(hu.pd.*seq.cps(1)) ...                 %SotR
+        .*(1-hu.ppc.*mdf.rahit).^(hu.pd.*seq.cps(3)) ...      %J
+        .*(1-hu.ppc.*mdf.rahit).^(hu.pd.*seq.cps(4));         %AS
     hu.p(1)=1-hu.q(1);
-    hu.q(2)=(1-hu.spc).^(hu.pd./cens.NetTick) ...
-        .*(1-hu.spc.*mdf.sphit).^(hu.pd.*seq.cps(5));
+    hu.q(2)=(1-hu.spc).^(hu.pd./cens.NetTick) ...     %Cens
+        .*(1-hu.spc.*mdf.sphit).^(hu.pd.*seq.cps(5)); %HW
     hu.p(2)=1-hu.q(2);
     hu.tm=zeros(4,4);
     hu.tm(1,1)=hu.q(1).*hu.q(2);hu.tm(1,2)=hu.p(1).*hu.q(2);hu.tm(1,3)=hu.p(2).*hu.q(1);hu.tm(1,4)=hu.p(1).*hu.p(2);
@@ -99,14 +106,14 @@ for i=1:5
     htrack=[htrack;hu.upt];
     gear.haste=hstore+900.*htrack(i,4);
     stat_model;ability_model;
-    hu.q(1)=(1-hu.ppc.*mdf.mehit).^(hu.pd./player.wswing) ...            %AA
-        .*(1-hu.ppc.*mdf.mehit).^(hu.pd.*3./(9+1.5.*rot.xtragcd)) ... %CS
-        .*(1-hu.ppc).^(hu.pd./(9+1.5.*rot.xtragcd)) ...               %SotR
-        .*(1-hu.ppc.*mdf.rahit).^(hu.pd./(9+1.5.*rot.xtragcd)) ...    %J
-        .*(1-hu.ppc.*mdf.rahit).^(hu.pd.*P.CS./(9+1.5.*rot.xtragcd)); %AS
+    hu.q(1)=(1-hu.ppc.*mdf.mehit).^(hu.pd./player.wswing) ... %AA
+        .*(1-hu.ppc.*mdf.mehit).^(hu.pd.*seq.cps(2)) ...      %CS
+        .*(1-hu.ppc).^(hu.pd.*seq.cps(1)) ...                 %SotR
+        .*(1-hu.ppc.*mdf.rahit).^(hu.pd.*seq.cps(3)) ...      %J
+        .*(1-hu.ppc.*mdf.rahit).^(hu.pd.*seq.cps(4));         %AS
     hu.p(1)=1-hu.q(1);
-    hu.q(2)=(1-hu.spc).^(hu.pd./cens.NetTick) ...
-        .*(1-hu.spc.*mdf.sphit).^(hu.pd.*max([0.75-P.CS;zeros(size(mdf.mehit))])./(9+1.5.*rot.xtragcd));
+    hu.q(2)=(1-hu.spc).^(hu.pd./cens.NetTick) ...     %Cens
+        .*(1-hu.spc.*mdf.sphit).^(hu.pd.*seq.cps(5)); %HW
     hu.p(2)=1-hu.q(2);
     hu.tm=zeros(4,4);
     hu.tm(1,1)=hu.q(1).*hu.q(2);hu.tm(1,2)=hu.p(1).*hu.q(2);hu.tm(1,3)=hu.p(2).*hu.q(1);hu.tm(1,4)=hu.p(1).*hu.p(2);
@@ -124,4 +131,6 @@ for j=0:450:900
     hu.dps=[hu.dps rot.totdps];
 end
 hu.dps=hu.dps(1).*(htrack(6,1)-1)+hu.dps(2).*sum(htrack(6,2:3))+hu.dps(3).*htrack(6,4);
-% [htrack(6,:) hu.dps]
+%cleanup
+gear.haste=hstore;
+clear i j htrack hstore
