85fe2ec0be602f1d8effa619f47dcbe3b5e643bd
theckhd@gmail.com <theckhd@gmail.com@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2011-02-03 15:46:10 +0000

    Major updates to [C*] to match new [RM] functionality and standardize format; New configs added to [DDB]

--- trunk/calc_talents.m
+++ trunk/calc_talents.m
@@ -12,11 +12,18 @@ egs=ddb.gearset{2}; %1=pre-raid , 2=raid
 glyph=ddb.glyphset{1}; %Default, HotR/SoT/ShoR, Cons/AS
 buff=buff_model;
 
+%need to run these here so that the cfg structure can set hit and exp
+gear_stats;
+talents;
+stat_model;
+ability_model;
+rotation_model;
+
 
 %% set up our tree configurations
 %base - unpossible build, contains a ridiculous # of points
 temptree.holy=[2 0 3 0; 0 0 0 0];
-temptree.prot=[3 2 2 0; 2 3 2 0; 2 3 1 2; 2 1 2 0; 1 1 2 1; 0 2 3 0; 0 1 0 0];
+temptree.prot=[3 2 2 2; 2 3 2 0; 2 3 1 2; 2 1 2 0; 1 1 2 1; 0 2 3 0; 0 1 0 0];
 temptree.ret=[0 3 2 0; 0 3 0 2];
 tree(1)=temptree;
 points(1)=1;
@@ -92,106 +99,155 @@ tree(k).prot(6,2)=0;
 points(k)=tree(1).prot(6,2);
 name{k}='Sacred Duty';
 
+%% Configurations
+%set melee hit to 2%, expertise to 10, mastery to 390 (16.5 mastery);
+%do this by altering helm stats
+cfg(1).helm=egs(1);
+cfg(1).helm.hit=max([egs(1).hit 0])-(player.phhit-2).*cnv.hit_phhit;
+cfg(1).helm.exp=max([egs(1).exp 0])-(player.exp-10).*cnv.exp_exp;
+cfg(1).helm.mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
+cfg(1).label='939/SoT';
+cfg(1).veng=1;
+cfg(1).seal='Truth';
+cfg(1).rot=1;
+cfg(1).glyph=ddb.glyphset{1}; %Default, HotR/SoT/ShoR, Cons/AS
+
+%low hit, W39
+cfg(2).helm=cfg(1).helm;
+cfg(2).label='W39/SoI';
+cfg(2).veng=1;
+cfg(2).seal='Insight';
+cfg(2).rot=7; %W39
+cfg(2).glyph=ddb.glyphset{2}; %WoG set
+
+%low hit, IHSH
+cfg(3)=cfg(1);
+cfg(3).label='IHSH/SoT';
+cfg(3).rot=2;
+
+%hit-cap and exp soft-cap, 939
+cfg(4)=cfg(2);
+cfg(4).label='939/SoT';
+cfg(4).helm=egs(1);
+cfg(4).helm.hit=max([egs(1).hit 0])-(player.phhit-8).*cnv.hit_phhit;
+cfg(4).helm.exp=max([egs(1).exp 0])-(player.exp-26).*cnv.exp_exp;
+cfg(4).helm.mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
 
 %% sim 
-
-%Calculate a sequence for this build, we'll use this sequence for all of
-%the talents that don't have a direct effect on the rotation 
-%(i.e. all but GC).
-% clear talent
-% talent=tree(1);
-% %invoke talents & glyphs
-% talents
-% %calculate relevant stats
-% gear_stats
-% %calculate final stats
-% stat_model
-% ability_model
-% rotation_model
-% 
-% %now calculate total DPS
-% totdps(1)=rot.totdps;
-% totdps1(1)=rot1.totdps;
-% totdps
-
-for m=1:length(tree) %everything
-   
-
-    clear talent
-    talent=tree(m);
-    %invoke talents & glyphs
-    talents;
-    %calculate relevant stats
-    gear_stats;
+tabledps=zeros(length(tree),length(rot),2);
+for c=1:length(cfg)
     
-    %artificially inflating hit and expertise to 8% and 26
-%     stat_conversions;stat_model;
-%     gear.hit=8*cnv.hit_phhit;
-%     gear.exp=(26-10-base.exp)*cnv.exp_exp;
+    for m=1:length(tree) %each talent
+        %configuration variables
+        exec=execution_model('veng',cfg(c).veng,'seal',cfg(c).seal);
+        egs(1)=cfg(c).helm;
+        glyph=cfg(c).glyph;
 
-    %calculate final stats
-    exec.npccount=1; %to reset from AoE
-    stat_model;
-    ability_model;
-    rotation_model;
-    
-    totdps(m)=rot(1).totdps;
-    totdps1(m)=rot(2).totdps;
-    
-    %calculate final stats
-    exec.npccount=4; %to reset from AoE
-    stat_model;
-    ability_model;
-    rotation_model;
-    totdpsa(m)=rot(6).totdps;
-    
+
+        clear talent
+        talent=tree(m);
+        %invoke talents & glyphs
+        talents;
+        %calculate relevant stats
+        gear_stats;
+
+        %calculate final stats
+        exec.npccount=1; %to reset from AoE
+        stat_model;
+        ability_model;
+        rotation_model;
+        
+        %store items in cfg for plots
+        cfg(c).hit=player.phhit;
+        cfg(c).exp=player.exp;
+        
+        totdps(m,c)=rot(cfg(c).rot).totdps;
+        totdps0(m,c)=totdps(1,c);
+
+        %calculate aoe stats
+        exec.npccount=4; 
+        stat_model;
+        ability_model;
+        rotation_model;
+        totdpsa(m,c)=rot(6).totdps;
+        totdpsa0(m,c)=totdpsa(1,c);
+
+    end
 end
 
 
-dpsppt=(totdps(1)-totdps')./points';
-dpsppt1=(totdps1(1)-totdps1')./points';
-dpsppta=(totdpsa(1)-totdpsa')./points';
+dpsppt=(totdps0-totdps)./repmat(points',1,size(totdps,2));
+% dpsppt1=(totdps1(1)-totdps1')./points';
+dpsppta=(totdpsa0-totdpsa)./repmat(points',1,size(totdpsa,2));
 
 %% table output
 spacer=repmat(' ',length(name),5);
-[char(name)  spacer num2str(dpsppt,'%2.1f') spacer num2str(dpsppt1,'%2.1f') spacer num2str(dpsppta,'%2.1f')]
+table_st=char(name);
+for c=1:length(cfg)
+    table_st=[table_st spacer num2str(dpsppt(:,c),'%2.1f')];
+end
+table_st
+
+table_aoe=char(name);
+for c=2:length(cfg)
+    table_aoe=[table_aoe spacer num2str(dpsppta(:,c),'%2.1f')];
+end
+table_aoe
 
 
 %% plots
-dpsplot=[dpsppt dpsppt1 dpsppta];
+dpsplot=[dpsppt(:,1:3)];
+aoeplot=[dpsppta(:,1:2)];
 
-figure(30)
-set(gcf,'Position',[428 128 728 378])
-bar30=barh(dpsplot(2:length(dpsplot),:),'BarWidth',1,'BarLayout','grouped');
-set(bar30(2),'FaceColor',[0.749 0.749 0]);
-ylim([0.5 10.5])
-set(gca,'YTickLabel',name(2:length(name)))
-legend('SCSC','IHSH','IHIH (AoE)','Location','Best')
-xlabel('DPS per point')
-title([ num2str(player.phhit,'%2.1f') '% hit, ' num2str(player.exp,'%2.1f') ' expertise'])
+% figure(30)
+% set(gcf,'Position',[428 128 728 378])
+% bar30=barh(dpsplot(2:length(dpsplot),:),'BarWidth',1,'BarLayout','grouped');
+% set(bar30(2),'FaceColor',[0.749 0.749 0]);
+% ylim([0.5 10.5])
+% set(gca,'YTickLabel',name(2:length(name)))
+% legend('SCSC','IHSH','IHIH (AoE)','Location','Best')
+% xlabel('DPS per point')
+% title([ num2str(player.phhit,'%2.1f') '% hit, ' num2str(player.exp,'%2.1f') ' expertise'])
 
-%sorted
-[dpspptsorted ind]=sort(dpsppt);
+%Sort
+sortby=1; %sort by cfg(1)
+[dpspptsorted ind]=sort(dpsppt(:,sortby));
 dpsplotsorted=dpsplot(ind,:);
-figure(31)
 
+[aoesorted inda]=sort(dpsppta(:,sortby));
+aoeplotsorted=aoeplot(inda,:);
+
+figure(31)
 set(gcf,'Position',[428 128 728 378])
 bar31=barh(dpsplotsorted(2:length(dpsppt),:),'BarWidth',1,'BarLayout','grouped');
 set(bar31(2),'FaceColor',[0.749 0.749 0]);
 ylim([0.5 10.5])
 set(gca,'YTickLabel',name(ind(2:length(name))))
-legend('SCSC','IHSH','IHIH (AoE)','Location','Best')
+legend({cfg.label},'Location','Best')
 xlabel('DPS per point')
-title([ num2str(exec.veng*100,'%2.1f') '% Veng, ' num2str(player.phhit,'%2.1f') '% hit, ' num2str(player.exp,'%2.1f') ' expertise'])
+title([ num2str(cfg(sortby).veng*100,'%2.1f') '% Veng, ' num2str(cfg(sortby).hit,'%2.1f') '% hit, ' num2str(cfg(sortby).exp,'%2.1f') ' expertise'])
 
 %for talent spec guide
 figure(32)
 set(gcf,'Position',[428 128 728 378])
-bar31=barh(dpsplotsorted(2:length(dpsppt),1),'BarWidth',0.5,'BarLayout','grouped');
-% set(bar31(2),'FaceColor',[0.749 0.749 0]);
+bar32=barh(dpspptsorted(2:length(dpspptsorted)),'BarWidth',0.5,'BarLayout','grouped');
+% bar32=barh(dpsplotsorted(2:length(dpspptsorted),1:2),'BarWidth',1,'BarLayout','grouped');
+% set(bar32(2),'FaceColor',[0.749 0.749 0]);
 ylim([0.5 10.5])
 set(gca,'YTickLabel',name(ind(2:length(name))))
-legend('SCSC','Location','Best')
+% legend({cfg.label},'Location','Best')
 xlabel('DPS per point')
-title([ num2str(exec.veng*100,'%2.1f') '% Veng, ' num2str(player.phhit,'%2.1f') '% hit, ' ...
-    num2str(player.exp,'%2.1f') ' expertise'])
+title([ num2str(cfg(sortby).veng*100,'%2.1f') '% Veng, ' num2str(cfg(sortby).hit,'%2.1f') '% hit, ' ...
+    num2str(cfg(sortby).exp,'%2.1f') ' expertise'])
+
+%aoe figure
+figure(33)
+set(gcf,'Position',[428 128 728 378])
+bar33=barh(aoeplotsorted(2:length(dpsppt),:),'BarWidth',1,'BarLayout','grouped');
+set(bar33(2),'FaceColor',[0.749 0.749 0]);
+% set(bar33(2),'FaceColor',[ 0.078 0.169 0.549]);
+ylim([0.5 10.5])
+set(gca,'YTickLabel',name(inda(2:length(name))))
+legend('IHSH/SoT','IHSH/SoI','Location','Best')
+xlabel('DPS per point')
+title(['4 targets, ' num2str(cfg(sortby).veng*100,'%2.1f') '% Veng, ' num2str(cfg(sortby).hit,'%2.1f') '% hit, ' num2str(cfg(sortby).exp,'%2.1f') ' expertise'])
--- trunk/calc_weapons.m
+++ trunk/calc_weapons.m
@@ -1,32 +1,23 @@
+%CALC_WEAPONS calculates the relative DPS output of different weapons
+
+%% Setup tasks
 clear;
 gear_db;
 def_db;
 
-exec=execution_model('veng',0.6);
+exec=execution_model('veng',1); 
 base=player_model('race','Belf','prof','');
 npc=npc_model(base);
 egs=ddb.gearset{2};  %1=pre-raid , 2=raid
 gear_stats;
-talent=ddb.talentset{2};  %0/31/10 w/HG
-glyph=ddb.glyphset{1}; %all relevant glyphs
+talent=ddb.talentset{1};  %placeholder, redefined in cfg
+glyph=ddb.glyphset{1}; %placeholder, redefined in cfg
 talents;
 buff=buff_model;
 stat_model;
 
-%adjustments to make sure that nothing is capped
-%set melee hit to 4%, expertise to 18, mastery to 390 (16.5 mastery);
-%do this by altering helm stats
-egs(1).hit=max([egs(1).hit 0])-(player.phhit-4).*cnv.hit_phhit;
-egs(1).exp=max([egs(1).exp 0])-(player.exp-18).*cnv.exp_exp;
-egs(1).mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
-helm1=egs(1);
 
-%repeat for 8% hit and exp soft-cap
-egs(1).hit=max([egs(1).hit 0])-(player.phhit-8).*cnv.hit_phhit;
-egs(1).exp=max([egs(1).exp 0])-(player.exp-26).*cnv.exp_exp;
-egs(1).mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
-helm2=egs(1);
-
+%% List of weapons
 weaplist1=[62457; %Ravening Slicer
            65173; %Thief''s Blade (Heroic)
            56346; %Elementium Fang (Heroic)
@@ -40,7 +31,6 @@ weaplist1=[62457; %Ravening Slicer
            59521; %Soul Blade
            65094; %Fang of Twilight (Heroic)
            65036; %Mace of Acrid Death (Heroic)
-           65103; %Soul Blade (Heroic)
           ];
 
 weaplist2=[56433; %Blade of the Burning Sun (Heroic)
@@ -51,7 +41,6 @@ weaplist2=[56433; %Blade of the Burning Sun (Heroic)
            59463; %Maldo''s Sword Cane
            59459; %Andoros, Fist of the Dragon King
            63680; %Twilight''s Hammer
-           65013; %Maldo''s Sword Cane (Heroic)
            65017; %Andoros, Fist of the Dragon King (Heroic)
            65090; %Twilight''s Hammer (Heroic)
           ];
@@ -79,53 +68,81 @@ M=length(weaplist);  %total number of weapons
 M1=length(weaplist1);M2=length(weaplist2);M3=length(weaplist3);
 
 
+%% Configurations
+%W39 rotation, low-hit set
+%set melee hit to 2%, expertise to 10, mastery to 390 (16.5 mastery);
+%do this by altering helm stats
+cfg(1).helm=egs(1);
+cfg(1).helm.hit=max([egs(1).hit 0])-(player.phhit-2).*cnv.hit_phhit;
+cfg(1).helm.exp=max([egs(1).exp 0])-(player.exp-10).*cnv.exp_exp;
+cfg(1).helm.mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
+cfg(1).rot=7;   %W39
+cfg(1).glyph=ddb.glyphset{2}; %WoG/SoI/HotR, AS/Cons
+cfg(1).talent=ddb.talentset{2}; %0/31/10 survivability
+cfg(1).label='W39/SoI build';
+
+%939 rotation, low-hit set
+cfg(2).helm=cfg(1).helm;
+cfg(2).rot=1;
+cfg(2).glyph=ddb.glyphset{1}; %SoT/SotR/HotR, AS/Cons
+cfg(2).talent=ddb.talentset{1}; %0/31/10 no HG
+cfg(2).label='939/SoT build';
+
+%939 for 8% hit and exp soft-cap
+cfg(3)=cfg(2);
+cfg(3).helm=egs(1);
+cfg(3).helm.hit=max([egs(1).hit 0])-(player.phhit-8).*cnv.hit_phhit;
+cfg(3).helm.exp=max([egs(1).exp 0])-(player.exp-26).*cnv.exp_exp;
+cfg(3).helm.mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
+cfg(3).label='939/SoT build, hit/exp cap';
 
 for m=1:M
     %equip the appropriate weapon
     egs(15)=equip(weaplist(m));
+    
     %store useful info for plots
     pinfo.name{m}=egs(15).name;
     pinfo.ilvl(m)=egs(15).ilvl;
-    
-    %calculate DPS below caps
-    egs(1)=helm1;
-    gear_stats;  
-    stat_model;
-    ability_model;
-    rotation_model;
 
-    dps1(m,1)=rot(1).totdps;
-    dps2(m,1)=rot(2).totdps;
-    
-    
-    %calculate DPS at caps
-    egs(1)=helm2;
-    gear_stats; 
-    stat_model;
-    ability_model;
-    rotation_model;
 
-    dps1(m,2)=rot(1).totdps;
-    dps2(m,2)=rot(2).totdps;
+    for c=1:length(cfg)
+
+
+
+
+        %calculate DPS below caps
+        egs(1)=cfg(c).helm;
+        glyph=cfg(c).glyph;
+        talent=cfg(c).talent;
+        talents;
+        gear_stats;
+        stat_model;
+        ability_model;
+        rotation_model;
+
+        wdps(m,c)=rot(cfg(c).rot).totdps;
+        wdps2(m,c)=rot(3).totdps; %for 9H9 / hammer check
+    end
+
 end
 
 pinfo.name=char(pinfo.name);
 pinfo.labels=[pinfo.name repmat(' ',length(pinfo.ilvl),2) int2str(pinfo.ilvl')];
 
 %HotR table
-dps=dps2;
+% dps=dps2;
 'HotR table'
-[pinfo.labels repmat(' ',length(dps),3) int2str(round(dps))]
+[pinfo.labels repmat(' ',length(wdps2),3) int2str(round(wdps2(:,2:3)))]
 
 
-dps=dps1;
+% dps=dps1;
 'CS table'
-[pinfo.labels repmat(' ',length(dps),3) int2str(round(dps))]
+[pinfo.labels repmat(' ',length(wdps),3) int2str(round(wdps))]
 
 y1=1:M1;y2=M1+1+[1:M2];y3=max(y2)+1+[1:M3];
 y=[y1 y2 y3];
 
-dps1p=[dps1(:,1) diff(dps1')']./1000;
+dps1p=[wdps(:,2) diff(wdps(:,2:3)')']./1000;
 
 figure(70)
 % set(gcf,'Position',[2200 12 724 936])
@@ -151,15 +168,15 @@ xlabel('DPS (thousands)')
 title('Tank weapons, sorted by DPS')
 legend('4% hit 18 exp','8% hit 26 exp','Location','Best')
 
-dps2p=[dps2(:,1) diff(dps2')'];
-
-figure(72)
-% set(gcf,'Position',[2200 12 724 936])
-bar72=barh(y,dps2p,'BarWidth',0.5,'BarLayout','stacked');
-set(bar72(2),'FaceColor',[0.749 0.749 0]);
-xlim([0.99.*min(min(dps2)) 1.01.*max(max(dps2))])
-ylim(0.5+[0 max(y)])
-set(gca,'YTick',y,'YTickLabel',pinfo.labels)
-xlabel('DPS (thousands)')
-title('All by ilvl & category - HotR rotation')
-legend('4% hit 18 exp','8% hit 26 exp','Location','Best')
+% dps2p=[dps2(:,1) diff(dps2')'];
+% 
+% figure(72)
+% % set(gcf,'Position',[2200 12 724 936])
+% bar72=barh(y,dps2p,'BarWidth',0.5,'BarLayout','stacked');
+% set(bar72(2),'FaceColor',[0.749 0.749 0]);
+% xlim([0.99.*min(min(dps2)) 1.01.*max(max(dps2))])
+% ylim(0.5+[0 max(y)])
+% set(gca,'YTick',y,'YTickLabel',pinfo.labels)
+% xlabel('DPS (thousands)')
+% title('All by ilvl & category - HotR rotation')
+% legend('4% hit 18 exp','8% hit 26 exp','Location','Best')
