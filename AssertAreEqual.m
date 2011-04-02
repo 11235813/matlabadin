1a3e45863aba0a2475c21077993fbdad79325f61
iminmmnni <iminmmnni@gmail.com>
2011-04-02 04:48:55 +0000

    fsm.m: initial implementation of CD finite state machine rotation calculator 	calling fsm without any argument will execute the fsm unit tests AssertAreEqual.m: helper function for unit tests perftest.m: performance benchmarking
    Note that the transition matrix does not appear to be positive determinate. How best do we find an Ax=b solution for the state transitions?

---
+++ AssertAreEqual.m
@@ -0,0 +1,40 @@
+%    Prot Pal Rotation state machine
+%    Copyright (C) 2011 Daniel Cameron
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+function AssertAreEqual (expected, actual, customErrorMessage = "")
+	if isfloat(expected)
+		if expected - actual != 0
+			% check if they're close enough
+			if abs((expected - actual)/min([expected, actual])) > 1e-10 % floating point tolerance
+				raiseError(expected, actual, customErrorMessage);
+			end
+		end
+		return
+	end
+	if length(expected) != length(actual)
+		raiseError(expected, actual, customErrorMessage);
+	else if not(all(expected == actual))
+		raiseError(expected, actual, customErrorMessage);
+	end
+end
+function raiseError (expected, actual, customErrorMessage)
+	if strcmp(customErrorMessage, "")
+		expected
+		actual
+		error("expected and actual are not equal");
+	else
+		error(customErrorMessage);
+	end
+end
