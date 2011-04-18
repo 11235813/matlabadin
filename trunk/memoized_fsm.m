1f64dc1cfcf846009ad25429ff8d27878bb351fe
theckhd@gmail.com <theckhd@gmail.com@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2011-04-18 14:12:54 +0000

    Bugfixes, added strsplit, shortened file name conventions.

--- trunk/memoized_fsm.m
+++ trunk/memoized_fsm.m
@@ -7,7 +7,9 @@ function  [actionPr, avgDuration, metadata] = memoized_fsm(rotation, mehit, rhit
 	else
 		strConsGlyph = 'false';
 	end
-	key = sprintf('fsmdata_%s_3_%f_%f_%s_%f_%f_%f', rotation, mehit, rhit, strConsGlyph, egProcRate, sdProcRate, gcProcRate);
+	key = sprintf('fsmdata_%s_3_%0.4f_%0.4f_%s_%g_%g_%g', rotation, mehit, rhit, strConsGlyph(1), round(egProcRate./0.15), round(sdProcRate./0.25), round(gcProcRate./0.10));
+    key = strrep(key,'_0.','_');
+    key = strrep(key,'_1.0000','_0000');
 	key = strrep(key, '.', '_');
 	key = strrep(key, '>', '_');
 	key = strrep(key, '*', 'star');
@@ -20,21 +22,21 @@ function  [actionPr, avgDuration, metadata] = memoized_fsm(rotation, mehit, rhit
 		return;
 	end
 	filename = strcat('data\\', key, '.csv');
-	if exist(filename) != 2
-		if exist('RotationCalculator') != 7
+	if exist(filename) ~= 2
+		if exist('RotationCalculator') ~= 7
 			pwd
 			error('Please set your working directory to the matlabadin root');
 		end
 		executable = 'RotationCalculator\Matlabadin\bin\Release\Matlabadin.exe';
-		if exist(executable) != 2
+		if exist(executable) ~= 2
 			executable = 'fsm.exe';
-			if exist(executable) != 2
+			if exist(executable) ~= 2
 				% Attempt to compile
 				% try 32 bit
 				system('%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\csc.exe /o+ /debug- /out:fsm.exe RotationCalculator\Matlabadin\*.cs >nul 2>&1');
 				% then overwrite with 64 bit if available
 				system('%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\csc.exe /o+ /debug- /out:fsm.exe RotationCalculator\Matlabadin\*.cs >nul 2>&1');
-				if exist(executable) != 2
+				if exist(executable) ~= 2
 					error('Please install version 4 of the .NET framework or mono. .NET 4 can be downloaded from: http://www.microsoft.com/downloads/en/details.aspx?FamilyID=5765d7a8-7722-4888-a970-ac39b33fd8ab&displaylang=en');
 				end
 			end			
@@ -50,20 +52,20 @@ function  [actionPr, avgDuration, metadata] = memoized_fsm(rotation, mehit, rhit
 	fsm_cache_metadata.(key) = metadata;
 end
 function [actionPr, avgDuration, metadata] = load_fsm_csv(filename)
-	fid = fopen(filename, "rt");
+	fid = fopen(filename, 'rt');
 	i = 1;
 	metadata = {};
 	while not(feof(fid))
 		txt = fgetl(fid);
-		rowEntry = strsplit(txt, ',', true);
+		rowEntry = strsplit(txt, ',');
 		lineAction = rowEntry{1};
 		lineTxtPr = rowEntry{2};
 		linePr = str2double(lineTxtPr);
 		if strcmp(lineAction, 'AvgDuration')
 			avgDuration = linePr;
-		elseif length(lineAction) > 6 && strcmp('Stats_', substr(lineAction, 1, 6))
+		elseif length(lineAction) > 6 && strcmp('Stats_', lineAction(1:6))
 			metadata.(lineAction) = lineTxtPr;
-		elseif length(lineAction) > 6 && strcmp('Param_', substr(lineAction, 1, 6))
+		elseif length(lineAction) > 6 && strcmp('Param_', lineAction(1:6))
 			metadata.(lineAction) = lineTxtPr;
 		else
 			actionPr{1, i} = lineAction;
