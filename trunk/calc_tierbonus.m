087995b8c1029b9105abc5b74bdb01f7330f12ad
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2012-01-17 17:58:26 +0000

    minor modifications to [CTB]

--- trunk/calc_tierbonus.m
+++ trunk/calc_tierbonus.m
@@ -9,7 +9,7 @@ def_db;
 exec=execution_model('veng',1); 
 base=player_model('race','Human','prof','');
 npc=npc_model(base);
-egs=ddb.gearset{5};  %3=T11H, 4=T12, 5=T12H
+egs=ddb.gearset{7};  %3=T11H, 4=T12, 5=T12H
 gear_stats;
 talent=ddb.talentset{1};
 glyph=ddb.glyphset{4}; 
@@ -20,7 +20,8 @@ stat_model;
 
 %% List of weapons
 weaplist=[ % 70922; %Mandible of Beth'tilac
-            71406; %Mandible of Beth'tilac (Heroic)
+%             71406; %Mandible of Beth'tilac (Heroic)
+            77193; %Souldrinker 
          ];
 M=length(weaplist);
 
@@ -32,14 +33,29 @@ V=length(vlist);
 %939 rotation, low-hit set
 %set melee hit to 2%, expertise to 10, mastery to 390 (16.5 mastery);
 %do this by altering helm stats
+
+queue.rot={ 'SotR>CS>J>AS';
+            'SotR>CS>AS+>J>AS';
+            'SotR>CS>AS+>J>AS>Cons';
+            'SotR>CS>AS+>J>AS>Cons>HW';
+            'ISotR>SDSotR>Inq>CS>J>AS';
+            'ISotR>SDSotR>Inq>CS>J>AS>Cons';
+            'ISotR>SDSotR>Inq>CS>J>AS>Cons>HW'};
 c=1;
 cfg(c).helm=egs(1);
 cfg(c).helm.hit=max([egs(1).hit 0])-(player.phhit-2).*cnv.hit_phhit;
 cfg(c).helm.exp=max([egs(1).exp 0])-(player.exp-10).*cnv.exp_exp;
-cfg(c).helm.mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
 cfg(c).rot=1; %939   
 cfg(c).seal='SoT';
 cfg(c).label='939/SoT, 2% hit, 10 exp';
+
+%939 for 8% hit and exp soft-cap
+c=c+1;
+cfg(c)=cfg(c-1);
+cfg(c).helm=egs(1);
+cfg(c).helm.hit=max([egs(1).hit 0])-(player.phhit-8).*cnv.hit_phhit;
+cfg(c).helm.exp=max([egs(1).exp 0])-(player.exp-26).*cnv.exp_exp;
+cfg(c).label='939/SoT, 8% hit, 26 exp';
 
 %939 for 8% hit and exp hard-cap
 c=c+1;
@@ -47,7 +63,6 @@ cfg(c)=cfg(c-1);
 cfg(c).helm=egs(1);
 cfg(c).helm.hit=max([egs(1).hit 0])-(player.phhit-8).*cnv.hit_phhit;
 cfg(c).helm.exp=max([egs(1).exp 0])-(player.exp-56).*cnv.exp_exp;
-cfg(c).helm.mast=max([egs(1).mast 0])-(player.mast-16.5).*cnv.mast_mast;
 cfg(c).label='939/SoT, 8% hit, 56 exp';
 
 % %I39 for 2% hit 10 exp
@@ -91,9 +106,10 @@ for m=1:M
             stat_model;
             ability_model;
             rotation_model;
-            
+            tmprot.dps=rot(cfg(c).rot).totdps;
+            dynamic_model;
             %store baseline
-            dps0(v,c,m)=rot(cfg(c).rot).totdps;
+            dps0(v,c,m)=tmprot.dps+proc.dps;
             
             
             %calculate each tier bonus
@@ -108,8 +124,9 @@ for m=1:M
                 stat_model;
                 ability_model;
                 rotation_model;
-                
-                prot_2pc_dps(t,v,c,m)=rot(cfg(c).rot).totdps;
+                tmprot.dps=rot(cfg(c).rot).totdps;
+                dynamic_model;
+                prot_2pc_dps(t,v,c,m)=tmprot.dps+proc.dps;
             end
             
             
@@ -123,8 +140,9 @@ for m=1:M
                 stat_model;
                 ability_model;
                 rotation_model;
-                
-                ret_2pc_dps(t,v,c,m)=rot(cfg(c).rot).totdps;
+                tmprot.dps=rot(cfg(c).rot).totdps;
+                dynamic_model;
+                ret_2pc_dps(t,v,c,m)=tmprot.dps+proc.dps;
             end
             
             
@@ -140,20 +158,31 @@ spacer = repmat(' ',4,3);
 for c=1:length(cfg)
     for m=1:M
         
-        tablecm=round([[prot_2pc_dps(:,2,c,m)';ret_2pc_dps(:,2,c,m)']-dps0(2,c,m) ...
-            [prot_2pc_dps(:,1,c,m)';ret_2pc_dps(:,1,c,m)']-dps0(1,c,m)]);
+        tablecm=round([prot_2pc_dps(:,2,c,m)';ret_2pc_dps(:,2,c,m)']-dps0(2,c,m));
         
         pptable=[char({' ',' ','prot 2pc','ret 2pc'}) spacer ...
-                 char({'v=','T11',int2str(tablecm(:,1))}) spacer ...
-                 char({num2str(vlist(2)),'T12',int2str(tablecm(:,2))}) spacer ...
+                 char({' ','T11',int2str(tablecm(:,1))}) spacer ...
+                 char({' ','T12',int2str(tablecm(:,2))}) spacer ...
                  char({' ','T13',int2str(tablecm(:,3))}) spacer ...
-                 char({'v=','T11',int2str(tablecm(:,4))}) spacer ...
-                 char({num2str(vlist(1)),'T12',int2str(tablecm(:,5))}) spacer ...
-                 char({' ','T13',int2str(tablecm(:,6))}) spacer ...
                  ];
+
+             
+%         %uncomment if using 30% vengeance as well
+%         tablecm=round([[prot_2pc_dps(:,2,c,m)';ret_2pc_dps(:,2,c,m)']-dps0(2,c,m) ...
+%             [prot_2pc_dps(:,1,c,m)';ret_2pc_dps(:,1,c,m)']-dps0(1,c,m)]);
+%         
+%         pptable=[char({' ',' ','prot 2pc','ret 2pc'}) spacer ...
+%                  char({'v=','T11',int2str(tablecm(:,1))}) spacer ...
+%                  char({[num2str(vlist(2)*100) '%'],'T12',int2str(tablecm(:,2))}) spacer ...
+%                  char({' ','T13',int2str(tablecm(:,3))}) spacer ...
+%                  char({'v=','T11',int2str(tablecm(:,4))}) spacer ...
+%                  char({[num2str(vlist(1)*100) '%'],'T12',int2str(tablecm(:,5))}) spacer ...
+%                  char({' ','T13',int2str(tablecm(:,6))}) spacer ...
+%                  ];
              
              disp(' ')
-             disp([cfg(c).label ', ' pinfo.name{m} ' ' int2str(pinfo.ilvl(m))])
+             disp([cfg(c).label ', ' queue.rot{cfg(c).rot}])
+             disp([pinfo.name{m} ' ' int2str(pinfo.ilvl(m))])
              disp(pptable)
             
     end
