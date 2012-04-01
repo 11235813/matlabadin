5543b9dac3644a7efc4ce6b544583ea1a172c2d1
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2012-04-01 04:07:20 +0000

    [CW] completed

--- calc_weapons.m
+++ calc_weapons.m
@@ -34,7 +34,7 @@ wids=weaplist(:,1);
 wcat=weaplist(:,2);
           
 M=length(wids);  %total number of weapons
-M1=sum(wcat==1);M2=sum(wcat==2);M3=sum(wcat==3); %length of each individual section
+N(1)=sum(wcat==1);N(2)=sum(wcat==2);N(3)=sum(wcat==3); %length of each individual section
 
 
 %% Crank
@@ -46,9 +46,6 @@ for g=1:length(cfg)
     c=stat_model(c);
     c=ability_model(c);
     c=rotation_model(c);
-    
-    dps0(:,g)=repmat(c.rot.dps,M,1);
-    hps0(:,g)=repmat(c.rot.dps,M,1);
     
     wb=waitbar(0,['Calculating Weapons']);
     tic
@@ -78,8 +75,8 @@ for g=1:length(cfg)
         c=ability_model(c);
         c=rotation_model(c);
         
-        dps(m,g)=c.rot.dps;
-        hps(m,g)=c.rot.hps;
+        wdps(m,g)=c.rot.dps;
+        whps(m,g)=c.rot.hps;
         waitbar(m/M,wb)
         
     end
@@ -89,94 +86,94 @@ end
 
 toc
 
-wdps=dps-dps0;
-whps=hps-hps0;
     
-% winfo.name=char(winfo.name);
-% winfo.labels=[winfo.name repmat(' ',length(winfo.ilvl),2) int2str(winfo.ilvl')];
-
-
 %% Table
-%TODO: Sorting
-ldat=2+[1:M];
+sortdata=[weaplist(:,2) winfo.ilvl];
+[ilvlsorted ind]=sortrows(sortdata);
+n=[0 cumsum(N)];
 
+%this constructs the table but divides it by weapon type, inserting a blank
+%line in between sections
 li=DataTable();
-li{1:2,1}={' ';'Weapon'};      
-li{ldat,1}=winfo.name;
-li{1:2,2}={' ';'ilvl'};      
-li{ldat,2}=winfo.ilvl;
-
-for g=1:length(cfg)
+for type=1:3
     
-li{1:2,2*g+1}={['cfg' int2str(g)];'DPS'};
-li{ldat,2*g+1}=round(wdps(:,g));
-
-li{1:2,2*g+2}={['cfg' int2str(g)];'HPS'};
-li{ldat,2*g+2}=round(whps(:,g));
-
-end     
+    %row indices for the table
+    ldat=2+ ...         %header
+        n(type)+ ...    %previous full rows
+        [1:N(type)]+... %new rows
+        (type-1);       %extra spaces    
+    
+    %indices of table for this weapon type
+    subind=ind(n(type)+[1:N(type)]);
+    
+    %fill in the name and ilvl columns
+    li{1:2,1}={' ';'Weapon'};
+    li{ldat,1}=winfo.name(subind);
+    li{1:2,2}={' ';'ilvl'};
+    li{ldat,2}=winfo.ilvl(subind);
+    
+    %fill in DPS columns for each configuration
+    for g=1:length(cfg)
+        
+        %DPS
+        li{1:2,2*g+1}={['cfg' int2str(g)];'DPS'};
+        li{ldat,2*g+1}=round(wdps(subind,g));
+        
+        %HPS
+        li{1:2,2*g+2}={['cfg' int2str(g)];'HPS'};
+        li{ldat,2*g+2}=round(whps(subind,g));
+        
+    end
+    
+end
 li.setColumnTextAlignment(1,'left')
 % li.setColumnFormat(3:4,'%6.0f')
 li.toText()
+    
 
-%% Plots
-% 
-% %number of weapons to skip in each category
-% m1=7;
-% m2=5;
-% m3=8;
-% m1=0;m2=0;m3=0;
-% 
-% %indices of desired weapons
-% i1=(1+m1):M1;
-% i2=M1+((1+m2):M2);
-% i3=M1+M2+((1+m3):M3);
-% 
-% %y-values for plots
-% y1=1:(M1-m1);
-% y3=(M1-m1)+1+(1:(M3-m3));
-% % y2=M1+1+[1:M2];
-% % y3=max(y2)+1+[1:M3];
-% 
-% %combine indices and y values from different sections
-% y=[y1 y3];
-% i=[i1 i3];
-% 
-% %construct DPS tables for plotting and sorting
-% dps70=[wdps(:,3) diff(wdps(:,3:4)')']./1000;
-% pinfo.plotlabels70=pinfo.labels(i,:);
-% dps70p=dps70(i,:);
-% xmin70=min(dps70p(:,1));
-% xmax70=max(sum(dps70p,2));
-% 
-% dps71=[wdps(i1,3) diff(wdps(i1,3:4)')']./1000;
-% [x71 k71]=sort(dps71(:,1));y71=y1(k71);
-% temp=pinfo.labels(i1,:);
-% pinfo.plotlabels71=temp(k71,:);
-% dps71p=dps71(k71,:);
-% xmin71=min(dps71p(:,1));
-% xmax71=max(sum(dps71p,2));
-% 
-% %All weapons plot
-% figure(70)
-% % set(gcf,'Position',[2200 12 724 936])
-% bar70=barh(y,dps70p,'BarWidth',0.5,'BarLayout','stacked');
-% set(bar70(2),'FaceColor',[0.749 0.749 0]);
-% xlim([0.99.*xmin70 1.01.*xmax70])
-% ylim(0.5+[0 max(y)])
-% set(gca,'YTick',y,'YTickLabel',pinfo.plotlabels70)
-% xlabel('DPS (thousands)')
-% title('All by ilvl & category - CS rotation')
-% legend('4% hit 18 exp','8% hit 26 exp','Location','Best')
-% 
-% 
-% figure(71)
-% % set(gcf,'Position',[2240 88 724 579])
-% bar71=barh(dps71p,'BarWidth',0.5,'BarLayout','stacked');
-% set(bar71(2),'FaceColor',[0.749 0.749 0]);
-% xlim([0.99.*xmin71 1.01.*xmax71])
-% ylim(0.5+[0 max(y1)])
-% set(gca,'YTick',y1,'YTickLabel',pinfo.plotlabels71)
-% xlabel('DPS (thousands)')
-% title('Tank weapons, sorted by DPS')
-% legend('4% hit 18 exp','8% hit 26 exp','Location','Best')
+%% Weapon plots, by section
+
+wtype={'Tank';'DPS';'Spell power'};
+for type=1:3
+    
+    subind=ind(n(type)+[1:N(type)]);
+    plotdps=[wdps(subind,1) diff(wdps(subind,:),1,2)]./1e3;
+    spacer=repmat(' ',N(type),1);
+    plotaxislabels=strcat([spacer],winfo.name(subind), ...
+                    [spacer spacer int2str(winfo.ilvl(subind))]);
+    
+    figure(70+type)
+    % set(gcf,'Position',[2240 88 724 579])
+    bar7x=barh(plotdps,'BarWidth',0.5,'BarLayout','stacked');
+    set(bar7x(2),'FaceColor',[0.749 0.749 0]);
+    xlim([0.99.*min(plotdps(:,1)) 1.01.*max(sum(plotdps,2))])
+    ylim(0.5+[0 N(type)])
+    set(gca,'YTick',1:N(type),'YTickLabel',plotaxislabels, ...
+            'OuterPosition',[0.01 0 0.99 1])
+    xlabel('DPS (thousands)')
+    title([char(wtype(type)) ' weapons, sorted by DPS'])
+    legend([num2str(cfg(1).player.mehit,'%g') '% hit, ' num2str(cfg(1).player.exp,'%g') '% exp'], ...
+       [num2str(cfg(2).player.mehit,'%g') '% hit, ' num2str(cfg(2).player.exp,'%g') '% exp'], ...
+        'Location','Best')
+
+end
+
+
+%% All weapons, sorted by DPS
+spacer=repmat(' ',M,1);
+allplotdps=[wdps(ind,1) diff(wdps(ind,:),1,2)]./1e3;
+allplotaxislabels=strcat([spacer],winfo.name(ind),[spacer spacer int2str(winfo.ilvl(ind))]);
+y74=[0 cumsum(diff(ilvlsorted(:,1)))']+(1:M);
+
+figure(74)
+bar74=barh(y74,allplotdps,'BarWidth',0.5,'BarLayout','stacked');
+set(bar74(2),'FaceColor',[0.749 0.749 0]);
+xlim([0.99.*min(allplotdps(:,1)) 1.01.*max(sum(allplotdps,2))])
+ylim(0.5+[0 max(y74)])
+set(gca,'YTick',y74,'YTickLabel',allplotaxislabels, ...
+    'OuterPosition',[0.01 0 0.99 1])
+xlabel('DPS (thousands)')
+title(['All weapons, sorted by type and DPS'])
+legend([num2str(cfg(1).player.mehit,'%g') '% hit, ' num2str(cfg(1).player.exp,'%g') '% exp'], ...
+       [num2str(cfg(2).player.mehit,'%g') '% hit, ' num2str(cfg(2).player.exp,'%g') '% exp'], ...
+        'Location','Best')
