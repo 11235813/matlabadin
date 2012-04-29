f07fb5e9aa54d48771f52a8b0c02feac606d9477
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2012-04-29 19:50:05 +0000

    Blog file update

--- blog/montecarlo.m
+++ blog/montecarlo.m
@@ -4,7 +4,7 @@ if nargin<4
     plotFlag='plot';
 end
 if nargin<3
-    simMins=1000;
+    simMins=500;
 end
 if nargin<2
     val=600;
@@ -35,8 +35,8 @@ haste=0.1+0.01.*dhaste./128.05715942;
 hit=0.02+0.01.*dhit./120.10880279;
 exp=0.02+0.01.*dexp./120.10880279;
 miss=0.075-hit;
-dodge=0.075-exp;
-parry=0.075-max((exp-0.075),0);
+dodge=max(0,(0.075-exp));
+parry=max(0,(0.075-max((exp-0.075),0)));
 hp=0;
 gcProcRate=0.2;
 gcBuffDuration=6.3;
@@ -49,10 +49,10 @@ tbe = zeros(size(events));
 idBossSwing=1;
 idGCD=2;
 
-buffs={'SotR buff #1 duration','SotR buff #2 duration','CS cooldown','J cooldown','SotR cooldown','Grand Crusader duration','Div Pupr duration'};
+buffs={'SotR duration','BoG Duration','CS cooldown','J cooldown','SotR cooldown','Grand Crusader duration','Div Pupr duration'};
 tob = zeros(size(buffs));
-idGBlock=1;
-idBvBuff=2;
+idSotR=1;
+idBoG=2;
 idCScd=3;
 idJcd=4;
 idSotRcd=5;
@@ -98,61 +98,76 @@ for k=1:N
                     if rand < avoidance
                         damage(k)=0;
                     else
-                        %figure out damage value.  First, check for gblock
-                        if tob(idGBlock)>0
-                            damage(k)=0.25;
-                            tob(idGBlock)=0;
-                        elseif rand < blockChance
-                            if tob(idBvBuff)>0
-                                damage(k)=0.5;
-                            else
-                                damage(k)=0.7;
-                            end
+                        %figure out damage value.  First, check for SotR
+                        if tob(idSotR)>0
+                            drmult=0.5;
                         else
-                            damage(k)=1;
+                            drmult=1;
+                        end
+                        %now check for block    
+                        if rand < blockChance
+                            damage(k)=0.7.*drmult;
+                        else
+                            damage(k)=1.*drmult;
                         end
                     end
-                                        
+                %if the GCD timer is up, see if there's something to cast                        
                 case idGCD
-                    
+                    %CS is first priority
                     if tob(idCScd)<=0
+                        %put CS on cooldown, start GCD
                         tob(idCScd)=4.5./(1+haste);
                         tbe(idGCD)=1.5./(1+haste);
+                        %Check for hit
                         if rand<(1-miss-dodge-parry)
+                            %success! grant HP, enforce bounds
                             hp=hp+1;hp=min([hp 5]);hp=max([hp 0]);
-                            debugHPG(k)=1;
-                            %grand crusader
+                            debugHPG(k)=1; %debug flag
+                            %Check for Grand Crusader proc
                             if rand<gcProcRate
+                                %set GC buff duration
                                 tob(idGCbuff)=gcBuffDuration;
                             end
                         end
+                    %J is second priority, check for cooldown and don't use
+                    %if CS cd is almost up
                     elseif tob(idJcd)<=0 && tob(idCScd)>=0.2
+                        %set J cooldown, start GCD
                         tob(idJcd)=6./(1+haste);
                         tbe(idGCD)=1.5./(1+haste);
+                        %check for hit
                         if rand<(1-miss-dodge)
+                            %success! grant HP, enforce bounds
                             hp=hp+1;hp=min([hp 5]);hp=max([hp 0]);
-                            debugHPG(k)=2;
+                            debugHPG(k)=2; %debug flag
                         end
-                    elseif tob(idGCbuff)>0
+                    %Grand Crusader
+                    elseif tob(idGCbuff)>0 && tob(idCScd)>=0.2
+                        %Consume GC buff, set GCD
                         tob(idGCbuff)=0;
                         tbe(idGCD)=1.5./(1+haste);
+                        %grant HP, enforce bounds
                         hp=hp+1;hp=min([hp 5]);hp=max([hp 0]);
-                            debugHPG(k)=3;
+                        debugHPG(k)=3; %debug flag
                     end
                     
-                    %check for 3+ HP, if so cast SotR
-                    if ( hp>=3 || tob(idDPBuff)>0 ) && tob(idSotRcd)<=0 && tob(idGBlock)<=0
+                    %check for 3+ HP and no SotR buff, if so cast SotR
+                    if ( hp>=3 || tob(idDPBuff)>0 ) && tob(idSotRcd)<=0 && tob(idSotR)<=0
+                        %set SotR CD, give 3 seconds of DR, 20s of BoG
                         tob(idSotRcd)=1.5;
-                        tob(idGBlock)=6;
-                        tob(idBvBuff)=max(0,tob(idBvBuff))+6;
+                        tob(idSotR)=3;
+                        tob(idBoG)=20;
+                        %Consume Divine Purpose proc if it exists
                         if tob(idDPBuff)>0
                            tob(idDPBuff)=0; 
+                        %otherwise, consume 3 HP
                         elseif hp>=3
                             hp=hp-3;
+                        %Just in case - debugging
                         else
                             error('WTF')
                         end
-                        %DP
+                        %Divine Purpose procs
                         if rand<dpProcRate
                             tob(idDPBuff)=dpBuffDuration;
                         end
@@ -166,8 +181,7 @@ for k=1:N
         
     
     %% Debugging
-    debugS(k,1)=tob(idBvBuff);
-    debugG(k,1)=tob(idGBlock);
+    debugS(k,1)=tob(idSotR);
     debugSotR(k,1)=tob(idSotRcd);
     debugHP(k,1)=hp;
     
@@ -178,41 +192,39 @@ end
 toc
 
 %% compile for plots
-avoids=sum(damage==0);
-gblocks=sum(damage==0.25);
-sblocks=sum(damage==0.5);
-rblocks=sum(damage==0.7);
-hits=sum(damage==1);
-allattacks=avoids+gblocks+sblocks+rblocks+hits;
+dmg=damage(find(damage>=0));
 
-avoidspct=avoids./allattacks;
-gblockspct=gblocks./allattacks;
-sblockspct=sblocks./allattacks;
-rblockspct=rblocks./allattacks;
-hitspct=hits./allattacks;
+S=sum(debugS>0)./length(debugS);
+avoids=sum(dmg==0);
+avoidspct=avoids./length(dmg);
+blocks=sum(dmg==0.7);
+blockspct=blocks./length(dmg);
+dr1=sum(dmg==0.5);
+dr1pct=dr1./length(dmg);
+dr2=sum(dmg==0.5.*0.7);
+dr2pct=dr2./length(dmg);
+hits=sum(dmg==1);
+hitspct=hits./length(dmg);
 
-G=gblocks./(allattacks-avoids);
-S=sblocks./(allattacks-avoids-gblocks-hits);
-Tsotr = max(t)./sum(debugG==6);
+
+Tsotr = max(t)./sum(debugS==3);
 Rsotr = 1/Tsotr;
 Rhpg=sum(debugHPG>0)/max(t);
 
-DTPS=sum((damage>0).*damage)./max(t);
+DTPS=sum(dmg)./max(t);
 
 if ~strcmp(plotFlag,'noplot')
 figure(1)
-x=[0 0.25 0.5 0.7 1]; y=[avoidspct gblockspct sblockspct rblockspct hitspct];
-bar(x,y)
+hist(dmg,[0:0.05:1])
 xlim([-0.1 1.1])
-text(-0.03,avoidspct+0.01,[num2str(avoidspct.*100,'%2.1f') '%'])
-text(0.25-0.03,gblockspct+0.01,[num2str(gblockspct.*100,'%2.1f') '%'])
-text(0.5-0.03,sblockspct+0.01,[num2str(sblockspct.*100,'%2.1f') '%'])
-text(0.7-0.03,rblockspct+0.01,[num2str(rblockspct.*100,'%2.1f') '%'])
-text(1-0.03,hitspct+0.01,[num2str(hitspct.*100,'%2.1f') '%'])
-title(['T=' int2str(simTime./60) ' min, G=' num2str(G.*100,'%2.1f') '%, S=' num2str(S.*100,'%2.1f') '%, Tsotr=' num2str(Tsotr,'%2.1f') ', DTPS=' num2str(DTPS.*100,'%2.2f')])
+text(-0.05,avoids.*1.05,[num2str(avoidspct.*100,'%2.1f') '%'])
+text(0.7-0.05,blocks.*1.05,[num2str(blockspct.*100,'%2.1f') '%'])
+text(0.5-0.05,dr1.*1.05,[num2str(dr1pct.*100,'%2.1f') '%'])
+text(0.7.*0.5-0.05,dr2.*1.05,[num2str(dr2pct.*100,'%2.1f') '%'])
+text(1-0.05,hits.*1.05,[num2str(hitspct.*100,'%2.1f') '%'])
+title(['T=' int2str(simTime./60) ' min, S=' num2str(S.*100,'%2.1f') '%, Tsotr=' num2str(Tsotr,'%2.1f') ', DTPS=' num2str(DTPS.*100,'%2.2f')])
 end
 
-statblock.G=G;
 statblock.S=S;
 statblock.Tsotr=Tsotr;
 statblock.Rhpg=Rhpg;
