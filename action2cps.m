d56c3b049dd3f4ec40bfae691b826ad388da8115
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2012-12-09 16:16:23 +0000

    HaNova proccing SoI bugfix

--- action2cps.m
+++ action2cps.m
@@ -166,6 +166,10 @@ for m=1:size(c.rot.actionPr,2)
             idx=find(strcmpi('HaNova',c.abil.val.label));
             cps(idx)=cpsval;
             ecpsd(idx)=cpsval.*emodd;
+            if sealidx==find(strcmpi('SoI',c.abil.val.label)) %HaNova can proc SoI for each target
+                cps(sealidx)=cps(sealidx)+cpsval;
+                ecpsh(sealidx)=ecpsh(sealidx)+cpsval.*emodh.*c.mdf.mehit(jme).*c.exec.npccount;
+            end
         case 'J'
             %seals (only procs SoT), hpg
             if sealidx==find(strcmpi('SoT',c.abil.val.label))
