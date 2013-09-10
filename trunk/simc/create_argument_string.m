b1862b9d232a945b3fdf009169c9553222340c9f
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2013-09-10 01:59:15 +0000

    Executable call construction, directory structure, etc.

--- trunk/simc/create_argument_string.m
+++ trunk/simc/create_argument_string.m
@@ -1,19 +1,22 @@
 function [ arg_str ] = create_argument_string( sim )
-%create_output_string takes the output file specifications provided in the
+%create_argument_string takes the output file specifications provided in the
 %"sim" argument and constructs the string passed to simc.exe on the
 %command line
 
 
 arg_str = '';
 
+%iterations
 if isfield(sim,'iterations') && ~isempty(sim.iterations)
     arg_str=strcat(arg_str,' iterations=',int2str(sim.iterations));
 end
 
-if isfield(sim,'player') && ~isempty(sim.player)
-    arg_str=strcat(arg_str,' ',sim.player);
+%ptr flag
+if isfield(sim,'ptr') && ~isempty(sim.ptr) && sim.ptr>0
+    arg_str=strcat(arg_str,' ptr=',int2str(sim.ptr));
 end
 
+%add trailing space
 if ~isempty(arg_str)
     arg_str=strcat(arg_str,' ');
 end
--- trunk/simc/run_sim.m
+++ trunk/simc/run_sim.m
@@ -2,27 +2,36 @@ function run_sim( sim )
 %RUN_SIM constructs the command-line simc.exe call using the information
 %passed in the "sim" structure
 
-%% Unpack sim structure
-simc_path = sim.path;
-if simc_path(length(simc_path)) ~= '\'; simc_path = [ simc_path '\' ]; end
+%% Simulation setup
+% make sure path is appropriately terminated
+if sim.path(length(sim.path)) ~= '\'; sim.path = [ sim.path '\' ]; end
 
+%find current path so that player simc files can be loaded
+if exist([sim.path 'rd\'],'dir') ~= 7
+    %get current path
+    s=what;
+    sim.playerpath=s.path;
+end
 
 %% arguments 
-sim.arguments = create_argument_string( sim );
+sim.argstr = create_argument_string( sim );
+
+%% player parameters
+sim.playerstr = create_player_string( sim );
 
 %% outputs
-sim.outputs = create_output_string( sim );
+sim.outputstr = create_output_string( sim );
 
 %%
 
 
 % check that we're in the correct folder and that simc exists
-if exist(simc_path,'dir') ~= 7 || exist(strcat(simc_path,sim.exe),'file') ~= 2
+if exist(sim.path,'dir') ~= 7 || exist(strcat(sim.path,sim.exe),'file') ~= 2
     error('SimC directory or executable cannot be found');
 end
 
 %% execute
-system(strcat(sim.path,sim.exe,sim.arguments,sim.outputs));
+system(strcat(sim.path,sim.exe,sim.argstr,sim.playerstr,sim.outputstr));
 
 end
 
