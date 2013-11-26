a10d27547883f2e5cab50fa256c6c5c601b954b3
theckhd <theckhd@e76f8986-cec8-cc6c-c222-359b3f6bbf6e>
2013-11-26 02:21:35 +0000

    calc_glyphs completed. May try to abstract the (currently) haphazard reporting section into its own thing eventually.

--- simc/calc_glyphs.m
+++ simc/calc_glyphs.m
@@ -8,6 +8,7 @@ sim=init_sim;
 sim.header='#Glyph Simulation';
 sim.iterations=10;
 sim.threads=4;
+sim.gear='T16N.simc';
 % sim.paths.exe='d:\simcraft\'
 %fix pdb path - in future may need to set this to "what" if we implement
 %\pdb\ in simc and still wish to use matlab path
@@ -15,16 +16,22 @@ sim = util_check_pdb_path(sim);
 glyphpath=[sim.paths.pdb 'glyphs\'];
 
 %% set up the list of things we want to vary
-glyph_pool={'empty';'empty';'alabaster_shield';'battle_healer';'divine_protection';'final_wrath';'focused_shield';'word_of_glory'};
+glyph_pool={'empty';'empty';'alabaster_shield';'avenging_wrath';'battle_healer';'devotion_aura';'divine_protection';'final_wrath';'focused_shield';'harsh_words';'immediate_truth';'word_of_glory'};
+glyph_abbr={'E';'E';'AS';'AW';'BH';'DA';'DP';'FW';'FS';'HW';'IT';'WoG'};
 glyph_combinations=cellstr('empty/empty/empty');
+glyph_abbreviated=cellstr('E_E_E');
 
-%this part creates every possible combination of the glyph pool... crudely
+%this part creates every possible combination of the glyph pool
 cell_index=2;
 for i=1:(length(glyph_pool)-2)
     for j=(i+1):(length(glyph_pool)-1)
         for k=(j+1):length(glyph_pool)
+            %glyph_combinations is the string that gets fed to simc
             glyph_combinations{cell_index}=...
-                strcat(glyph_pool{i},'/',glyph_pool{j},'/',glyph_pool{k}); %#ok<SAGROW>
+                strcat(glyph_pool{i},'/',glyph_pool{j},'/',glyph_pool{k}); 
+            %glyph_abbreviated is the short version
+            glyph_abbreviated{cell_index}=strcat(glyph_abbr{i},'_',glyph_abbr{j},'_',glyph_abbr{k});
+            %increment cell index
             cell_index=cell_index+1;
         end
     end
@@ -33,8 +40,7 @@ end
 
 %% create the talent .simc files we need
 for i=1:length(glyph_combinations)
-    glyph_combo_helper{i}=strrep(glyph_combinations{i},'/','_');
-    glyph_files{i}=create_simc_component(['glyphs=' glyph_combinations{i}],glyphpath,glyph_combo_helper{i});  %#ok<SAGROW>
+    glyph_files{i}=create_simc_component(['glyphs=' glyph_combinations{i}],glyphpath,glyph_abbreviated{i});  %#ok<SAGROW>
 end
 
 %% crank through them
@@ -54,11 +60,11 @@ for i=1:length(glyph_combinations);
     sim.glyphs=glyph_files{i};
     
     %rename simc file
-    sim.simc=strcat('io\glyph_',glyph_combo_helper{i},'.simc');
+    sim.simc=strcat('io\glyph_',glyph_abbreviated{i},'.simc');
     
     %set output filenames
-    sim.output.html=strcat('io\glyph_',glyph_combo_helper{i},'.html');
-    sim.output.output=strcat('io\glyph_',glyph_combo_helper{i},'.txt');
+    sim.output.html=strcat('io\glyph_',glyph_abbreviated{i},'.html');
+    sim.output.output=strcat('io\glyph_',glyph_abbreviated{i},'.txt');
     
     %construct simc fullpath
     fullpath=sf_construct_fullpaths(sim);
@@ -89,16 +95,16 @@ toc
 %% Compile results into usefully-formatted table
 addpath ./helper_func/
 dt=DataTable();
-dt{1,1}='Glyph1';
-dt{1,2}='Glyph2';
-dt{1,3}='Glyph3';
+dt{1,1}='G1';
+dt{1,2}='G2';
+dt{1,3}='G3';
 dt{1,4}='DPS';
 dt{1,5}='HPS';
 dt{1,6}='DTPS';
 dt{1,7}='TMI';
 dt{1,8}='SotR';
 for i=1:length(glyph_combinations)
-   names=regexp(glyph_combinations{i},'(?<g1>\w+)/(?<g2>\w+)/(?<g3>\w+)','names');
+   names=regexp(glyph_abbreviated{i},'(?<g1>\w+)_(?<g2>\w+)_(?<g3>\w+)','names');
    dt{1+i,1}=names.g1;
    dt{1+i,2}=names.g2;
    dt{1+i,3}=names.g3;
@@ -112,22 +118,26 @@ end
 
 disp(' ')
 disp('Full List')
+disp(['Max DPS Err: ' int2str(round(max([results.dps_error]))) ', Max DPS % Err: ' num2str(max([results.dps_pct_error]),'%2.2f') '%'])
+disp(['Max TMI Err: ' num2str(max([results.tmi_error]),'%5.2f') ', Max TMI % Err: ' num2str(max([results.tmi_pct_error]),'%2.2f') '%'])
 dt.toText()
 
 %% Compile short table of single-glyph results
 dts=DataTable();
 dts{1,1}='Glyph';
 dts{1,2}='DPS';
-dts{1,3}='dDPS';
-dts{1,4}='HPS';
-dts{1,5}='DTPS';
-dts{1,6}='TMI';
-dts{1,7}='SotR';
+dts{1,3}='DPSErr';
+dts{1,4}='dDPS';
+dts{1,5}='HPS';
+dts{1,6}='DTPS';
+dts{1,7}='TMI';
+dts{1,8}='SotR';
 for i=2:length(glyph_pool)
     %special formatting here, want some diffs
     dts{i,1:2}=dt.getData(i,3:4); %Glyph, DPS
-    dts{i,3}=str2num(char(dts.getData(i,2)))-str2num(char(dts.getData(2,2))); %#ok<ST2NM>
-    dts{i,4:size(dts,2)}=dt.getData(i,5:size(dt,2));
+    dts{i,3}=round(results(i).dps_error);
+    dts{i,4}=str2num(char(dts.getData(i,2)))-str2num(char(dts.getData(2,2))); %#ok<ST2NM>
+    dts{i,5:size(dts,2)}=dt.getData(i,5:size(dt,2));
 end
 
 disp(' ')
@@ -139,9 +149,18 @@ dts.toText()
 topDPS=dpsindex(1:10);
 
 dpst=DataTable();
-dpst{1,1:size(dt,2)}=dt.getData(1,1:size(dt,2));
+dpst{1,1:4}=dt.getData(1,1:4);
+dpst{1,5}='DPSErr';
+dpst{1,6}='DPS%Err';
+dpst{1,7}='HPS';
+dpst{1,8}='DTPS';
+dpst{1,9}='TMI';
+dpst{1,10}='SotR';
 for i=1:length(topDPS)
-    dpst{1+i,1:size(dt,2)}=dt.getData(1+topDPS(i),1:size(dt,2));
+    dpst{1+i,1:4}=dt.getData(1+topDPS(i),1:4);
+    dpst{1+i,5}=round(results(topDPS(i)).dps_error);
+    dpst{1+i,6}=[num2str(results(topDPS(i)).dps_pct_error,'%2.2f') '%'];
+    dpst{1+i,7:10}=dt.getData(1+topDPS(i),5:8);
 end
 
 disp(' ')
@@ -153,9 +172,15 @@ dpst.toText()
 topTMI=tmiindex(1:10);
 
 tmit=DataTable();
-tmit{1,1:size(dt,2)}=dt.getData(1,1:size(dt,2));
+tmit{1,1:7}=dt.getData(1,1:7);
+tmit{1,8}='TMIErr';
+tmit{1,9}='TMI%Err';
+tmit{1,10}='SotR';
 for i=1:length(topTMI)
-    tmit{1+i,1:size(dt,2)}=dt.getData(1+topTMI(i),1:size(dt,2));
+    tmit{1+i,1:7}=dt.getData(1+topTMI(i),1:7);
+    tmit{1+i,8}=num2str(results(topTMI(i)).tmi_error,'%5.2f');
+    tmit{1+i,9}=[num2str(results(topTMI(i)).tmi_pct_error,'%5.2f') '%'];
+    tmit{1+i,10}=dt.getData(1+topTMI(i),8);
 end
 
 disp(' ')
